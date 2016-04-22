99cc19576bc38b83e167b155d657cb4dab4574f4
Andrew Alexander <andrewalexander@users.noreply.github.com>
2016-04-22 15:25:50 -0400

    Update s3stat.py to properly deal with multi-region
    The results dict would show empty when the bucket was not in the same region that the boto3.Session() was established in. Not sure if it is a bug with cloudwatch or not, but this fix is working for my needs.

--- tools/s3stat.py
+++ tools/s3stat.py
@@ -68,20 +68,35 @@ def main():
    logging.basicConfig(level=logging.INFO)
    
    bucket = os.environ.get('BUCKET')
-   s = boto3.Session()
-   cw = s.client('cloudwatch')
-   s3 = s.client('s3')
-   buckets = s3.list_buckets()['Buckets'] 
-
+   
    results = {'buckets':[]}
    size_count = obj_count = 0.0
+   regions = ['eu-west-1',
+              'ap-southeast-1',
+              'ap-southeast-2',
+              'eu-central-1',
+              'ap-northeast-2',
+              'ap-northeast-1',
+              'us-east-1',
+              'sa-east-1',
+              'us-west-1',
+              'us-west-2']
+   for region in regions:
 
-   for b in buckets:
-      i = bucket_info(cw, b['Name'])
-      results['buckets'].append(i)
-      obj_count += i['ObjectCount']
-      size_count += i['SizeGB']
- 
+        s = boto3.Session(region_name=region)
+        cw = s.client('cloudwatch')
+        s3 = s.client('s3')
+        buckets = s3.list_buckets()['Buckets']
+
+        for b in buckets:
+              i = bucket_info(cw, b['Name'])
+              bucket_region = s3.get_bucket_location(Bucket=b['Name'])['LocationConstraint']
+              # bucket_region is `None` when it is in us-east-1 (US Standard)
+              if bucket_region == region or bucket_region == None:
+                results['buckets'].append(i)
+                obj_count += i['ObjectCount']
+                size_count += i['SizeGB']
+
    results['TotalObjects'] = obj_count
    results['TotalSizeGB'] = size_count
 
