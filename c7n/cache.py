0b44614288603ea732aee2fad4063647503477c6
Faisal Dosani <faisal.dosani@gmail.com>
2016-04-23 16:52:27 -0400

    create cache folder if nonexistant

--- c7n/cache.py
+++ c7n/cache.py
@@ -27,11 +27,11 @@ log = logging.getLogger('c7n.cache')
 def factory(config):
     if not config:
         return NullCache(None)
-    
+
     if not config.cache or not config.cache_period:
-        log.info("Disabling cache")    
+        log.info("Disabling cache")
         return NullCache(config)
-    
+
     return FileCacheManager(config)
 
 
@@ -45,11 +45,11 @@ class NullCache(object):
 
     def get(self, key):
         pass
-    
+
     def save(self, key, data):
         pass
-    
-    
+
+
 class FileCacheManager(object):
 
     def __init__(self, config):
@@ -64,7 +64,7 @@ class FileCacheManager(object):
     def get(self, key):
         k = cPickle.dumps(key)
         return self.data.get(k)
-        
+
     def load(self):
         if os.path.isfile(self.cache_path):
             if (time.time() - os.stat(self.cache_path).st_mtime >
@@ -74,8 +74,13 @@ class FileCacheManager(object):
                 self.data = cPickle.load(fh)
             log.info("Using cache file %s" % self.cache_path)
             return True
-        
+
+
     def save(self, key, data):
+        if not os.path.exists(self.cache_path):
+            directory = os.path.dirname(self.cache_path)
+            log.info('Cache path does not exists: %s. Generating' % directory)
+            os.makedirs(directory)
         with open(self.cache_path, 'w') as fh:
             cPickle.dump({
                 cPickle.dumps(key): data}, fh, protocol=2)
