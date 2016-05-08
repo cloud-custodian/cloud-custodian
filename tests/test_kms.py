5edeeacfee20fa1b553938276a8a80f3f3c83486
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2016-05-07 21:20:33 -0400

    kms grant uses session replay

--- tests/test_kms.py
+++ tests/test_kms.py
@@ -4,12 +4,13 @@ from common import BaseTest
 class KMSTest(BaseTest):
 
     def test_kms_grant(self):
-        self.replay_flight_data('test_kms_grants', zdata=True)
+        session_factory = self.replay_flight_data('test_kms_grants')
         p = self.load_policy(
             {'name': 'kms-grant-count',
              'resource': 'kms',
              'filters': [
-                 {'type': 'grant-count'}]})
+                 {'type': 'grant-count'}]},
+            session_factory=session_factory)
 
         resources = p.run()
         self.assertEqual(len(resources), 0)
