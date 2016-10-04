67c5012237cbd535b38fe6f6df3c838128450bd7
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2016-10-03 20:59:16 -0400

    model annotation for config support, additional resource types

--- c7n/resources/__init__.py
+++ c7n/resources/__init__.py
@@ -36,7 +36,6 @@ def load_resources():
     import c7n.resources.elasticsearch
     import c7n.resources.elb
     import c7n.resources.emr
-    import c7n.resources.firehose
     import c7n.resources.glacier
     import c7n.resources.iam
     import c7n.resources.kinesis
@@ -49,3 +48,7 @@ def load_resources():
     import c7n.resources.sns
     import c7n.resources.sqs
     import c7n.resources.vpc
+
+    # Load external plugins (private sdks etc)
+    from c7n.manager import resources
+    resources.load_plugins()
