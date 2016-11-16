dab6312577aeec28372a0c292b02afe0f89ab764
Scot Kronenfeld <scot@sixfeetup.com>
2016-11-15 22:02:04 -0500

    For policy renaming tool, adding in local renaming #397

--- tools/ops/policyrename.py
+++ tools/ops/policyrename.py
@@ -14,24 +14,31 @@
 """Policy output rename utility
 """
 
+from __future__ import print_function
+
 import argparse
+import fileinput
 import logging
+import os
+import sys
 
 from boto3.session import Session
 from botocore.exceptions import ClientError
 from c7n.utils import parse_s3
 
+
 log = logging.getLogger("custodian.policyrename")
+
+
+class ArgumentError(Exception): pass
 
 
 def setup_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--output-dir", "-s", required=True,
+    parser.add_argument('-s', '--output-dir', required=True,
                         help="Directory or S3 URL For Policy Output")
-    parser.add_argument("old",
-                        help="Old policy name")
-    parser.add_argument("new",
-                        help="New policy name")
+    parser.add_argument("old", help="Old policy name")
+    parser.add_argument("new", help="New policy name")
 
     return parser
 
@@ -76,6 +83,54 @@ def s3_rename(output_dir, old, new):
         log.debug('Deleted "{}"'.format(old_key))
 
 
+def local_rename(output_dir, old, new):
+    """ Rename a local filesystem directory to a new name.
+    
+    Merge over existing files if applicable.
+    """
+    old_dir = os.path.join(output_dir, old)
+    new_dir = os.path.join(output_dir, new)
+
+    # If the old_dir doesn't exist, there is nothing to do.
+    if not os.path.exists(old_dir):
+        raise ArgumentError("Error: There is no existing output for a policy with name.")
+
+    # If the new_dir doesn't exist yet, just rename the old one and we're done.
+    if not os.path.exists(new_dir):
+        os.rename(old_dir, new_dir)
+        print("Successfully renamed policy {} to {}.".format(old_dir, new_dir))
+        return
+        
+    # If there are files in both places we want to be more careful.  There is a
+    # log file that we want to concatenate together.  For all other files,
+    # we want to keep whatever is in the newer directory.
+    old_file = os.path.join(old_dir, 'custodian-run.log')
+    new_file = os.path.join(new_dir, 'custodian-run.log')
+    old_file_exists = os.path.exists(old_file)
+    new_file_exists = os.path.exists(new_file)
+    
+    if old_file_exists and new_file_exists:
+        tmp_file = os.path.join(old_dir, 'tmp-rename-{}.log'.format(os.getpid()))
+        with open(tmp_file, 'w') as fout:
+            for line in fileinput.input([old_file, new_file]):
+                fout.write(line)
+        os.rename(tmp_file, new_file)
+    elif old_file_exists:
+        shutil.copy(old_file, new_file)
+
+    # We could do a shutil.rmtree(old_dir) here, but there is lots of potential
+    # for Bad Things to happen if the user supplies bad inputs
+    # (e.g. if they give output-dir='/' and old='' we would do shutil.rmtree('/'))
+    #
+    # So we'll just leave the old directory alone. Note that it only gets left
+    # behind if old_dir *and* new_dir exist.  In the case where new_dir did not
+    # yet exist, old_dir would be renamed and nothing would be left behind.
+    print("Output files existed for both policies.  The log files were merged",
+          "and all files from the new directory were left in place.\n")
+    print("To delete the old policy output directory run:")
+    print("  rm -fr {}".format(old_dir))
+
+
 def main():
     parser = setup_parser()
     options = parser.parse_args()
@@ -85,11 +140,14 @@ def main():
         format="%(asctime)s: %(name)s:%(levelname)s %(message)s")
     logging.getLogger('botocore').setLevel(logging.ERROR)
 
-    if options.output_dir.startswith('s3://'):
-        s3_rename(options.output_dir, options.old, options.new)
-    else:
-        pass
-        #local_rename()
+    try:
+        if options.output_dir.startswith('s3://'):
+            s3_rename(options.output_dir, options.old, options.new)
+        else:
+            local_rename(options.output_dir, options.old, options.new)
+    except ArgumentError as e:
+        print(e.message)
+        sys.exit(2)
 
 
 if __name__ == '__main__':
