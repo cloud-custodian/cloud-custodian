e197b17b4f08c2b54c8c37642c941494138a87c5
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2016-12-06 16:31:33 -0500

    Updated rename to support buckets with over 1k keys * Added pagination support * Added support to copy the existing metadata for the logs * Added SSE-S3 and SSE-KMS support * Added support to re-encrypt in place with new keys

--- tools/ops/policyrename.py
+++ tools/ops/policyrename.py
@@ -17,10 +17,7 @@
 from __future__ import print_function
 
 import argparse
-import fileinput
 import logging
-import os
-import shutil
 import sys
 
 from boto3.session import Session
@@ -31,16 +28,22 @@ from c7n.utils import parse_s3
 log = logging.getLogger("custodian.policyrename")
 
 
-class ArgumentError(Exception): pass
+class ArgumentError(Exception):
+    pass
 
 
 def setup_parser():
     desc = ('This utility script will preserve the history of a policy '
             'if it is renamed.  Pass in the old policy name and new '
             'policy name and any old policy output and logs will be '
-            'copied to the new policy name.')
+            'copied to the new policy name. '
+            'This utility can also be used to re-encrypt your logs '
+            'by providing your SSE-KMS key. If the old and new targets '
+            'are the same, it will re-encrypt in-place')
 
     parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--sse-kms-key-id',
+                        help="Key id for SSE-KMS encrypted objects")
     parser.add_argument('-s', '--output-dir', required=True,
                         help="Directory or S3 URL For Policy Output")
     parser.add_argument("old", help="Old policy name")
@@ -49,55 +52,90 @@ def setup_parser():
     return parser
 
 
-def s3_rename(output_dir, old, new):
+def s3_rename(output_dir, old, new, sse_kms_key_id):
     # move the old data into the new area
     session = Session()
     client = session.client('s3')
     s3 = session.resource('s3')
     s3_path, bucket, key_prefix = parse_s3(output_dir)
-    
+
     # Ensure bucket exists
     try:
         client.head_bucket(Bucket=bucket)
     except ClientError:
         raise ArgumentError('S3 bucket {} does not exist.'.format(bucket))
 
-    try:
-        log.info(
-            'Retrieving list of S3 objects to rename in bucket "{}"'.format(
-                bucket
-            )
+    log.info(
+        'Retrieving list of S3 objects to rename in bucket "{}"'.format(
+            bucket
         )
-        to_rename = client.list_objects(Bucket=bucket, Prefix=old + '/')
-    except ClientError as e:
-        log.error(e.message)
+    )
+    paginator = client.get_paginator('list_objects_v2')
+    rename_iterator = paginator.paginate(Bucket=bucket, Prefix=old + '/')
 
-    if to_rename is None or to_rename.get('Contents') is None:
-        raise ArgumentError('Key {} does not exist in bucket {}'.format(
-                old, bucket))
+    for page in rename_iterator:
+        # loop through the pages of results renaming
 
-    # Loop through the old objects copying and deleting
-    for obj in to_rename.get('Contents'):
-        old_key = obj.get('Key')
-        new_key = new + old_key[len(old):]
+        if page.get('Contents') is None:
+            raise ArgumentError('Key {} does not exist in bucket {}'.format(
+                    old, bucket))
 
-        # check that we haven't already run and have existing data
-        # in the new key
-        new_obj = s3.Object(bucket, new_key)
-        try:
-            new_obj.load()
-            log.info('Skipping existing output in new location: {}'.format(
-                new_obj.key))
-        except ClientError as e:
-            response_code = e.response.get('Error').get('Code')
-            if response_code == '404':
-                new_obj.copy_from(
-                    CopySource={'Bucket': bucket, 'Key': old_key}
-                )
-                log.debug('Renamed "{}" to "{}"'.format(old_key, new_key))
-        # Either way, we delete the old object
-        s3.Object(bucket, old_key).delete()
-        log.debug('Deleted "{}"'.format(old_key))
+        # Loop through the old objects copying and deleting
+        for obj in page.get('Contents'):
+            old_key = obj.get('Key')
+            old_meta = client.head_object(Bucket=bucket, Key=old_key)
+            old_sse_type = old_meta.get('ServerSideEncryption')
+            old_sse_key = old_meta.get('SSEKMSKeyId')
+            new_key = new + old_key[len(old):]
+
+            # check that we haven't already run and have existing data
+            # in the new key
+            new_obj = s3.Object(bucket, new_key)
+            if new_key == old_key:
+                log.info(('Old and new keys match and new SSEKMSKeyId '
+                         'Specified, re-encrypting {}').format(new_obj.key))
+            else:
+                try:
+                    new_obj.load()
+                    if new_key != old_key:
+                        log.info('Skipping existing output in new '
+                                 'location: {}'.format(new_obj.key))
+                        continue
+                except ClientError as e:
+                    response_code = e.response.get('Error').get('Code')
+                    if response_code == '404':
+                        # the obj doesn't exist so we will copy
+                        # the existing obj to the new spot
+                        pass
+                    else:
+                        raise
+
+            copy_from_args = dict(
+                CopySource={
+                    'Bucket': bucket,
+                    'Key': old_key,
+                    'MetadataDirective': 'COPY'
+                })
+
+            if sse_kms_key_id:
+                # Re-encrypt with a new key
+                copy_from_args['ServerSideEncryption'] = 'aws:kms'
+                copy_from_args['SSEKMSKeyId'] = sse_kms_key_id
+            if not sse_kms_key_id and old_sse_type == 'aws:kms':
+                # Re-encrypt with the existing key
+                copy_from_args['ServerSideEncryption'] = 'aws:kms'
+                copy_from_args['SSEKMSKeyId'] = old_sse_key
+            if old_sse_type == 'AES256':
+                # Re-encrypt with the existing AES256
+                copy_from_args['ServerSideEncryption'] = 'AES256'
+
+            new_obj.copy_from(**copy_from_args)
+            log.debug('Renamed "{}" to "{}"'.format(old_key, new_key))
+            # Either way, we delete the old object unless we are inplace
+            # re-encrypting
+            if new_key != old_key:
+                s3.Object(bucket, old_key).delete()
+                log.debug('Deleted "{}"'.format(old_key))
 
 
 def main():
@@ -111,7 +149,10 @@ def main():
 
     if options.output_dir.startswith('s3://'):
         try:
-            s3_rename(options.output_dir, options.old, options.new)
+            s3_rename(options.output_dir,
+                      options.old,
+                      options.new,
+                      options.sse_kms_key_id)
         except ArgumentError as e:
             print(e.message)
             sys.exit(2)
