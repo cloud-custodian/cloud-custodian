61dfedd9c8069a80fc0eb5be390818c15a523d5b
Scot Kronenfeld <scot@sixfeetup.com>
2016-12-20 16:36:17 -0500

    Adding setuptools packaging for sphinxext tool, and adding it to the requirements-dev.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'tools.sphinxext.c7n_schema',
+    'c7n_sphinxext.c7n_schema',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
