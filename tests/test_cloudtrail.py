28aca96e6c44fc1ea7b5507cfe1c69a1d2014a12
Rob McBroom <rob@sixfeetup.com>
2017-02-14 10:52:23 -0500

    update enable trail tests

--- tests/test_cloudtrail.py
+++ tests/test_cloudtrail.py
@@ -2,10 +2,12 @@ from common import BaseTest
 from c7n.utils import local_session
 
 
+TRAIL = 'nosetest'
+
+
 class CloudTrail(BaseTest):
 
     def test_create(self):
-        p_trails = ['nosetest']
         factory = self.replay_flight_data('test_cloudtrail_create')
         p = self.load_policy(
             {
@@ -14,7 +16,8 @@ class CloudTrail(BaseTest):
                 'actions': [
                     {
                         'type': 'enable',
-                        'trails': p_trails,
+                        'trail': TRAIL,
+                        'bucket': '%s-bucket' % TRAIL,
                     },
                 ],
             },
@@ -22,7 +25,7 @@ class CloudTrail(BaseTest):
         )
         p.run()
         client = local_session(factory).client('cloudtrail')
-        resp = client.describe_trails(trailNameList=p_trails)
+        resp = client.describe_trails(trailNameList=[TRAIL])
         trails = resp['trailList']
         arn = trails[0]['TrailARN']
         status = client.get_trail_status(Name=arn)
@@ -37,14 +40,28 @@ class CloudTrail(BaseTest):
                 'actions': [
                     {
                         'type': 'enable',
-                        'trails': ['nosetest'],
+                        'trail': TRAIL,
+                        'bucket': '%s-bucket' % TRAIL,
+                        'multi-region': False,
+                        'global-events': False,
+                        'notify': 'test',
+                        'file-digest': True,
+                        'kms': True,
+                        'kms_key': 'arn:aws:kms:us-east-1:1234:key/fake',
                     },
                 ],
             },
             session_factory=factory,
         )
-        trails = p.run()
+        p.run()
         client = local_session(factory).client('cloudtrail')
-        arn = trails[0]['TrailARN']
+        resp = client.describe_trails(trailNameList=[TRAIL])
+        trails = resp['trailList']
+        test_trail = trails[0]
+        self.assertFalse(test_trail['IsMultiRegionTrail'])
+        self.assertFalse(test_trail['IncludeGlobalServiceEvents'])
+        self.assertTrue(test_trail['LogFileValidationEnabled'])
+        self.assertEqual(test_trail['SnsTopicName'], 'test')
+        arn = test_trail['TrailARN']
         status = client.get_trail_status(Name=arn)
         self.assertTrue(status['IsLogging'])
