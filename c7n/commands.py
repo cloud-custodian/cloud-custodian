a53df5c9524b04c4ce8b2241250a1418faa7c53d
Scot Kronenfeld <scot@sixfeetup.com>
2017-02-21 11:16:37 -0500

    When the policy file is empty, exit 1 for `logs` and `report` subcommands (#949)

--- c7n/commands.py
+++ c7n/commands.py
@@ -69,9 +69,11 @@ def policy_command(f):
 
         if len(policies) == 0:
             _print_no_policies_warning(options, all_policies)
-            if len(all_policies) == 0:
-                return
-            sys.exit(1)
+            # If we filtered out all the policies we want to exit with a
+            # non-zero status. But if the policy file is empty then continue
+            # on to the specific command to determine the exit status.
+            if len(all_policies) > 0:
+                sys.exit(1)
 
         # Do not allow multiple policies with the same name, even across files
         counts = Counter([p.name for p in policies])
@@ -106,10 +108,9 @@ def _print_no_policies_warning(options, policies):
 def validate(options):
     load_resources()
     if len(options.configs) < 1:
-        # no configs to test
-        # We don't have the parser object, so fake ArgumentParser.error
         eprint('Error: no config files specified')
         sys.exit(1)
+
     used_policy_names = set()
     schm = schema.generate()
     errors = []
@@ -186,8 +187,8 @@ def run(options, policies):
 
 @policy_command
 def report(options, policies):
-    if len(policies) > 1:
-        eprint("Error: Report subcommand can only operate on one policy")
+    if len(policies) != 1:
+        eprint("Error: Report subcommand requires exactly one policy")
         sys.exit(1)
     
     policy = policies.pop()
@@ -207,8 +208,8 @@ def report(options, policies):
 
 @policy_command
 def logs(options, policies):
-    if len(policies) > 1:
-        eprint("Error: Log subcommand can only operate on one policy")
+    if len(policies) != 1:
+        eprint("Error: Log subcommand requires exactly one policy")
         sys.exit(1)
 
     policy = policies.pop()
