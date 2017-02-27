f507126790be11336b39ca1d2f79a7b0160a9475
Chad Whitacre <chad@zetaweb.com>
2017-02-27 07:57:45 -0500

    Fix MemoryError in test_iam
    This test manually creates a `PythonPackageArchive`, and thus it
    benefiteth not from the "aggressive shrinking" of `custodian_archive`.
    Here we can be even more aggressive.

--- tests/test_iam.py
+++ tests/test_iam.py
@@ -511,7 +511,8 @@ class LambdaCrossAccount(BaseTest):
 
         tmp_dir = tempfile.mkdtemp()
         self.addCleanup(os.rmdir, tmp_dir)
-        archive = PythonPackageArchive(tmp_dir, tmp_dir)
+        exclude_all = lambda a,b,c: ([], [])
+        archive = PythonPackageArchive(tmp_dir, tmp_dir, lib_filter=exclude_all)
         archive.create()
         archive.add_contents('handler.py', LAMBDA_SRC)
         archive.close()
