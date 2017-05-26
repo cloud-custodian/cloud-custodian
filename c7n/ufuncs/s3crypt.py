cloud-custodian/cloud-custodian

commit 7f22f7e3c6b1dc46fa789dd7190b2a533b27bbe7

s3 action and mu - bucket sns notification support to attach-encrypt (#921)

c7n/ufuncs/s3crypt.py
@@ -65,13 +65,18 @@ def process_key_event(event, context):
         print("remediated %s:%s" % (bucket, key['Key']))
 
 
-def get_function(session_factory, role, buckets=None, account_id=None):
+def process_sns_event(event, context):
+    for record in event.get('Records', []):
+        process_key_event(json.loads(record['Sns']['Message']), context)
+
+
+def get_function(session_factory, role, via_sns, buckets=None, account_id=None):
     from c7n.mu import (
-        LambdaFunction, custodian_archive, BucketNotification)
+        LambdaFunction, custodian_archive, BucketLambdaNotification)
 
     config = dict(
         name='c7n-s3-encrypt',
-        handler='s3crypt.process_key_event',
+        handler='s3crypt.process_' + 'sns_event' if via_sns else 'key_event',
         memory_size=256,
         timeout=30,
         role=role,
@@ -80,7 +85,8 @@ def get_function(session_factory, role, buckets=None, account_id=None):
 
     if buckets:
         config['events'] = [
-            BucketNotification({'account_s3': account_id}, session_factory, b)
+            BucketLambdaNotification({'account_s3': account_id},
+                session_factory, b)
             for b in buckets]
 
     archive = custodian_archive()

commit 389748ca17f15acb0576c87a18e7bc8c07c80ad5

s3 - fix attach encrypt regression from #921 (#1230)

.travis.yml
@@ -14,6 +14,6 @@ install:
 script:
   - flake8
   - make sphinx
-  - py.test -v -n auto --cov=c7n tests tools/c7n_*
+  - py.test -n auto --cov=c7n tests tools/c7n_*
 after_success:
   coveralls

c7n/ufuncs/s3crypt.py
@@ -76,7 +76,7 @@ def get_function(session_factory, role, via_sns, buckets=None, account_id=None):
 
     config = dict(
         name='c7n-s3-encrypt',
-        handler='s3crypt.process_' + 'sns_event' if via_sns else 'key_event',
+        handler='s3crypt.process_%s' % (via_sns and 'sns_event' or 'key_event'),
         memory_size=256,
         timeout=30,
         role=role,