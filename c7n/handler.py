cloud-custodian/cloud-custodian

commit a44d3c24722b1297c346b224b63f0de8a93a9899

lambda policies - Allow setting execution options (cross account/region for periodic) (#1114)

c7n/handler.py
@@ -21,8 +21,10 @@ an event.
 import logging
 import os
 import uuid
+import json
 
-from c7n.policy import load
+from c7n.policy import PolicyCollection
+from c7n.resources import load_resources
 from c7n.utils import format_event, get_account_id_from_sts
 
 
@@ -80,7 +82,18 @@ def dispatch_event(event, context):
     if event['debug']:
         log.info("Processing event\n %s", format_event(event))
 
-    policies = load(Config.empty(), 'config.json', format='json')
+    # policies file should always be valid in lambda so do loading naively
+    with open('config.json') as f:
+        policy_config = json.load(f)
+
+    if not policy_config or not policy_config.get('policies'):
+        return True
+
+    options_overrides = policy_config['policies'][0]['mode'].get('execution-options', {})
+    options = Config.empty(**options_overrides)
+
+    load_resources()
+    policies = PolicyCollection(policy_config, options)
     if policies:
         for p in policies:
             p.push(event, context)

commit 641d7d523b90c358dc8d4476862fca08476eb646

fix trunk regression from #1114 on lambda policies

c7n/handler.py
@@ -87,13 +87,14 @@ def dispatch_event(event, context):
         policy_config = json.load(f)
 
     if not policy_config or not policy_config.get('policies'):
-        return True
+        return False
 
+    # TODO. This enshrines an assumption of a single policy per lambda.
     options_overrides = policy_config['policies'][0]['mode'].get('execution-options', {})
     options = Config.empty(**options_overrides)
 
     load_resources()
-    policies = PolicyCollection(policy_config, options)
+    policies = PolicyCollection.from_data(policy_config, options)
     if policies:
         for p in policies:
             p.push(event, context)

tools/c7n_logexporter/c7n_logexporter/exporter.py
@@ -99,6 +99,7 @@ def cli():
 @cli.command()
 @click.option('--config', type=click.Path())
 def validate(config):
+    """validate config file."""
     with open(config) as fh:
         content = fh.read()
 
@@ -124,6 +125,7 @@ def validate(config):
 @click.option('--end')
 @debug
 def run(config, start, end):
+    """run export across accounts and log groups specified in config."""
     config = validate.callback(config)
     destination = config.get('destination')
     start = start and parse(start) or start
@@ -348,6 +350,7 @@ def filter_extant_exports(client, bucket, prefix, days, start, end=None):
 # @click.option('--stream-prefix)
 @lambdafan
 def export(group, bucket, prefix, start, end, role, session=None):
+    """Export a single log group to s3."""
     start = start and isinstance(start, basestring) and parse(start) or start
     end = (end and isinstance(start, basestring) and
            parse(end) or end or datetime.now())