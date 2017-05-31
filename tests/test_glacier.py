5203284969614cec46b187859df525acabb2e6dc
alfredgamulo <alfred.gamulo@gmail.com>
2017-05-31 11:13:35 -0400

    removing unecessary policy and adding functional decorator

--- tests/test_glacier.py
+++ tests/test_glacier.py
@@ -1,10 +1,24 @@
-from common import BaseTest
+# Copyright 2016 Capital One Services, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from common import BaseTest, functional
 from c7n.executor import MainThreadExecutor
 from c7n.resources.glacier import Glacier
 
-
 class GlacierTagTest(BaseTest):
 
+    @functional
     def test_glacier_tag(self):
         session_factory = self.replay_flight_data('test_glacier_tag')
         client = session_factory().client('glacier')
@@ -12,21 +26,6 @@ class GlacierTagTest(BaseTest):
 
         client.create_vault(vaultName=name)
         self.addCleanup(client.delete_vault, vaultName=name)
-
-        p = self.load_policy({
-            'name': 'glacier',
-            'resource': 'glacier',
-            'filters': [
-                {
-                    'type': 'value',
-                    'key': 'VaultName',
-                    'value': 'c7n-glacier-test'
-                }
-            ]},
-            session_factory=session_factory)
-        resources = p.run()
-        self.assertEqual(len(resources), 1)
-        self.assertEqual(resources[0]['VaultName'], name)
 
         p = self.load_policy({
             'name': 'glacier',
@@ -65,19 +64,6 @@ class GlacierTagTest(BaseTest):
                 {
                     'tag:abc': 'present'
                 }
-            ]},
-            session_factory=session_factory)
-        resources = p.run()
-        self.assertEqual(len(resources), 1)
-        self.assertEqual(len(resources[0]['Tags']), 1)
-
-        p = self.load_policy({
-            'name': 'glacier',
-            'resource': 'glacier',
-            'filters': [
-                {
-                    'tag:abc': 'present'
-                }
             ],
             'actions': [
                 {
@@ -96,22 +82,6 @@ class GlacierTagTest(BaseTest):
         session_factory = self.replay_flight_data('test_glacier_markop')
         client = session_factory().client('glacier')
         name = 'c7n-glacier-test'
-
-        # client.create_vault(vaultName=name)
-        # self.addCleanup(client.delete_vault, vaultName=name)
-
-        p = self.load_policy({
-            'name': 'glacier',
-            'resource': 'glacier',
-            'filters': [
-                {
-                    'tag:abc': 'present'
-                }
-            ]},
-            session_factory=session_factory)
-        resources = p.run()
-        self.assertEqual(len(resources), 1)
-        self.assertEqual(len(resources[0]['Tags']), 1)
 
         p = self.load_policy({
             'name': 'glacier',
