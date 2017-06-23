bd44b6066efd7e835a857cd9212b8ae90a308302
alfredgamulo <alfred.gamulo@gmail.com>
2017-06-23 12:52:08 -0400

    [WIP] use universal_taggable to add tags to resources, also add universal_augment

--- c7n/query.py
+++ c7n/query.py
@@ -29,7 +29,7 @@ from concurrent.futures import as_completed
 
 from c7n.actions import ActionRegistry
 from c7n.filters import FilterRegistry, MetricsFilter
-from c7n.tags import register_tags
+from c7n.tags import register_ec2_tags, register_universal_tags
 from c7n.utils import (
     local_session, generate_arn, get_retry, chunks, camelResource)
 from c7n.registry import PluginRegistry
@@ -109,7 +109,7 @@ class QueryMeta(type):
         if attrs['resource_type']:
             m = ResourceQuery.resolve(attrs['resource_type'])
             # Generic cloud watch metrics support
-            if m.dimension and 'metrics':
+            if m.dimension:
                 attrs['filter_registry'].register('metrics', MetricsFilter)
             # EC2 Service boilerplate ...
             if m.service == 'ec2':
@@ -118,8 +118,11 @@ class QueryMeta(type):
                     'RequestLimitExceeded', 'Client.RequestLimitExceeded')))
                 # Generic ec2 resource tag support
                 if getattr(m, 'taggable', True):
-                    register_tags(
+                    register_ec2_tags(
                         attrs['filter_registry'], attrs['action_registry'])
+            if getattr(m, 'universal_taggable', False):
+                register_universal_tags(
+                    attrs['filter_registry'], attrs['action_registry'])
         return super(QueryMeta, cls).__new__(cls, name, parents, attrs)
 
 
