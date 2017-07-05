f58552cb8315dae097873d31456500d03d7be059
Jamison Roberts <Jamison.T.Roberts@transamerica.com>
2017-07-05 18:10:17 -0500

    fixed indent issue

--- c7n/mu.py
+++ c7n/mu.py
@@ -816,7 +816,7 @@ class CloudWatchEventSource(object):
         cwevents = self.data.get('events')
         payload = {}
         if cwevents is None:
-            #'events is None'
+            continue
         else:
             for e in cwevents:
                 if not isinstance(e, dict):
