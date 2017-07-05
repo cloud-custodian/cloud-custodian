5b16a068ab1a310e61c9ceeacaeae2b36ba99c52
Jamison Roberts <jtroberts83@users.noreply.github.com>
2017-07-05 14:54:14 -0500

    Update mu.py to allow for root console login event
    In order to get a policy working that triggers on a CloudTrail console
    signin event I edited the mu.py code to provide the correct detail-type
    payload for the CWERule as its different for console signins vs other
    API actions.  I have tested this and it works

--- c7n/mu.py
+++ c7n/mu.py
@@ -813,8 +813,16 @@ class CloudWatchEventSource(object):
     def render_event_pattern(self):
         event_type = self.data.get('type')
         payload = {}
-        if event_type == 'cloudtrail':
-            payload['detail-type'] = ['AWS API Call via CloudTrail']
+        sources = self.data.get('sources', [])
+        for e in  self.data.get('events'):
+            e = json.dumps(e)
+            sources.append(e['source'])
+        payload = {}
+        if event_type == 'cloudtrail' and 'signin.amazonaws.com' in sources:
+            payload['detail-type'] = ['AWS Console Sign In via CloudTrail']
+            self.resolve_cloudtrail_payload(payload)
+        elif event_type == 'cloudtrail':
+            payload['detail-type'] = ['AWS API Call via CloudTrail']   
             self.resolve_cloudtrail_payload(payload)
 
         elif event_type == "ec2-instance-state":
