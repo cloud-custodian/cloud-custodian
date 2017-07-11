cloud-custodian/cloud-custodian

commit 1d6f82357b4106755952e39160740b2ae3cec4cb

update utils for Python 3
* chuck returns lists, so the tests now convert range() to a list for comparison
* calling list() on map() just to force iteration

c7n/utils.py
@@ -76,7 +76,7 @@ def load_file(path, format=None, vars=None):
                 msg = 'Failed to substitute variable by positional argument.'
                 raise VarsSubstitutionError(msg)
             except KeyError as e:
-                msg = 'Failed to substitute variables.  KeyError on "{}"'.format(e.message)
+                msg = 'Failed to substitute variables.  KeyError on {}'.format(str(e))
                 raise VarsSubstitutionError(msg)
 
         if format == 'yaml':
@@ -187,13 +187,13 @@ def camelResource(obj):
     """
     if not isinstance(obj, dict):
         return obj
-    for k in list(obj.keys()):
+    for k in obj.keys():
         v = obj.pop(k)
         obj["%s%s" % (k[0].upper(), k[1:])] = v
         if isinstance(v, dict):
             camelResource(v)
         elif isinstance(v, list):
-            map(camelResource, v)
+            list(map(camelResource, v))
     return obj
 
 

tests/test_utils.py
@@ -138,13 +138,16 @@ class UtilTest(unittest.TestCase):
     def test_chunks(self):
         self.assertEqual(
             list(utils.chunks(range(100), size=50)),
-            [range(50), range(50, 100, 1)])
+            [list(range(50)), list(range(50, 100, 1))],
+        )
         self.assertEqual(
             list(utils.chunks(range(1), size=50)),
-            [range(1)])
+            [[0]],
+        )
         self.assertEqual(
             list(utils.chunks(range(60), size=50)),
-            [range(50), range(50, 60, 1)])
+            [list(range(50)), list(range(50, 60, 1))],
+        )
 
     def test_type_schema(self):
         self.assertEqual(

commit f0fca3adc246a6d1f0dfb0e3182ac904f65922af

update revisions for Python 3
* copy keys before camel-casing, since they get changed in the loop (broken in 1d6f82357b4106755952e39160740b2ae3cec4cb)
* add a mask to CRC32 to ensure it always returns the same positive number in Python 2 and 3
* adjust diff order in the tests to match results based on the new CRC32 hashes

c7n/resources/vpc.py
@@ -324,7 +324,7 @@ class SecurityGroupDiff(object):
             ev.sort()
             for e in ev:
                 buf += "%s-" % e
-        return abs(zlib.crc32(buf.encode('ascii')))
+        return zlib.crc32(buf.encode('ascii')) & 0xffffffff
 
 
 @SecurityGroup.action_registry.register('patch')

c7n/utils.py
@@ -187,7 +187,7 @@ def camelResource(obj):
     """
     if not isinstance(obj, dict):
         return obj
-    for k in obj.keys():
+    for k in list(obj.keys()):
         v = obj.pop(k)
         obj["%s%s" % (k[0].upper(), k[1:])] = v
         if isinstance(v, dict):

tests/test_revisions.py
@@ -49,7 +49,14 @@ class SGDiffLibTest(BaseTest):
                              u'PrefixListIds': [],
                              u'UserIdGroupPairs': []}]},
              'ingress': {
-                 'added': [{u'FromPort': 8485,
+                 'added': [{u'FromPort': 22,
+                            u'IpProtocol': u'tcp',
+                            u'IpRanges': [{u'CidrIp': u'10.0.0.0/24'}],
+                            u'Ipv6Ranges': [],
+                            u'PrefixListIds': [],
+                            u'ToPort': 22,
+                            u'UserIdGroupPairs': []},
+                           {u'FromPort': 8485,
                             u'IpProtocol': u'tcp',
                             u'IpRanges': [],
                             u'Ipv6Ranges': [],
@@ -57,13 +64,7 @@ class SGDiffLibTest(BaseTest):
                             u'ToPort': 8485,
                             u'UserIdGroupPairs': [{u'GroupId': u'sg-a38ed1de',
                                                    u'UserId': u'644160558196'}]},
-                           {u'FromPort': 22,
-                            u'IpProtocol': u'tcp',
-                            u'IpRanges': [{u'CidrIp': u'10.0.0.0/24'}],
-                            u'Ipv6Ranges': [],
-                            u'PrefixListIds': [],
-                            u'ToPort': 22,
-                            u'UserIdGroupPairs': []}]},
+                        ]},
              'tags': {'added': {u'Scope': u'account'}}})
 
     def test_sg_patch_pitr(self):