9daaafb803585adbeb8744b90d054a2eeb0807d4
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2017-09-06 20:48:45 -0400

    try to fix doc sphinx issue

--- c7n/resources/iam.py
+++ c7n/resources/iam.py
@@ -836,44 +836,44 @@ class UserDelete(BaseAction):
 
     ... code-block: yaml
 
-     # using a 'credential' filter':
-     - name: iam-only-whitelisted-users
-       resource: iam-user
-       filters:
-         - type: credential
-           key: user
-           op: not-in
-           value:
-             - valid-user-1
-             - valid-user-2
-       actions:
-         - delete
+      # using a 'credential' filter':
+      - name: iam-only-whitelisted-users
+        resource: iam-user
+        filters:
+          - type: credential
+            key: user
+            op: not-in
+            value:
+              - valid-user-1
+              - valid-user-2
+        actions:
+          - delete
 
-     # using a 'username' filter with 'UserName':
-     - name: iam-only-whitelisted-users
-       resource: iam-user
-       filters:
-         - type: username
-           key: UserName
-           op: not-in
-           value:
-             - valid-user-1
-             - valid-user-2
-       actions:
-         - delete
+      # using a 'username' filter with 'UserName':
+      - name: iam-only-whitelisted-users
+        resource: iam-user
+        filters:
+          - type: username
+            key: UserName
+            op: not-in
+            value:
+              - valid-user-1
+              - valid-user-2
+        actions:
+          - delete
 
-     # using a 'username' filter with 'Arn':
-     - name: iam-only-whitelisted-users
-       resource: iam-user
-       filters:
-         - type: username
-           key: Arn
-           op: not-in
-           value:
-             - arn:aws:iam:123456789012:user/valid-user-1
-             - arn:aws:iam:123456789012:user/valid-user-2
-       actions:
-         - delete
+      # using a 'username' filter with 'Arn':
+      - name: iam-only-whitelisted-users
+        resource: iam-user
+        filters:
+          - type: username
+            key: Arn
+            op: not-in
+            value:
+              - arn:aws:iam:123456789012:user/valid-user-1
+              - arn:aws:iam:123456789012:user/valid-user-2
+        actions:
+          - delete
 
     """
     schema = type_schema('delete')
