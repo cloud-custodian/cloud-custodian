cloud-custodian/cloud-custodian

commit a73a4c9ed1e2a29fda1d2bf1e485ec4e50d249aa

mu - better runtime configuration of lambda policies (#1638)

c7n/schema.py
@@ -185,6 +185,7 @@ def generate(resource_types=()):
         'policy-mode': {
             'type': 'object',
             'required': ['type'],
+            'additionalProperties': False,
             'properties': {
                 'type': {
                     'enum': [
@@ -202,8 +203,19 @@ def generate(resource_types=()):
                          'properties': {
                              'source': {'type': 'string'},
                              'ids': {'type': 'string'},
-                             'event': {'type': 'string'}}}]
-                }}
+                             'event': {'type': 'string'}}}],
+                }},
+
+                'role': {'type': 'string'},
+                'runtime': {'enum': ['python2.7', 'python3.6']},
+                'memory': {'type': 'number'},
+                'timeout': {'type': 'number'},
+
+                'dead_letter_config': {'type': 'object'},
+                'environment': {'type': 'object'},
+                'kms_key_arn': {'type': 'string'},
+                'tracing_config': {'type': 'object'},
+                'tags': {'type': 'object'},
             },
         },
     }

c7n/mu.py
@@ -26,7 +26,6 @@ import io
 import json
 import logging
 import os
-import sys
 import time
 import tempfile
 import zipfile
@@ -348,7 +347,6 @@ class LambdaManager(object):
 
             new_config = func.get_config()
             new_config['Role'] = role
-            del new_config['Runtime']
             new_tags = new_config.pop('Tags', {})
 
             if self.delta_function(old_config, new_config):
@@ -621,10 +619,9 @@ def run(event, context):
 
 
 class PolicyLambda(AbstractLambdaFunction):
-    """Wraps a custodian policy to turn it into lambda function.
+    """Wraps a custodian policy to turn it into a lambda function.
     """
     handler = "custodian_policy.run"
-    runtime = "python%d.%d" % sys.version_info[:2]
 
     def __init__(self, policy):
         self.policy = policy
@@ -642,6 +639,10 @@ class PolicyLambda(AbstractLambdaFunction):
     @property
     def role(self):
         return self.policy.data['mode'].get('role', '')
+
+    @property
+    def runtime(self):
+        return self.policy.data['mode'].get('runtime', 'python2.7')
 
     @property
     def memory_size(self):

tests/test_mu.py
@@ -28,12 +28,58 @@ import unittest
 import zipfile
 
 from c7n.mu import (
-    custodian_archive, LambdaManager, PolicyLambda, PythonPackageArchive,
+    custodian_archive, LambdaFunction, LambdaManager, PolicyLambda, PythonPackageArchive,
     CloudWatchLogSubscription, SNSSubscription)
 from c7n.policy import Policy
 from c7n.ufuncs import logsub
 from .common import BaseTest, Config, event_data
 from .data import helloworld
+
+
+ROLE = "arn:aws:iam::644160558196:role/custodian-mu"
+
+
+class Publish(BaseTest):
+
+    def make_func(self, **kw):
+        func_data = dict(
+            name='test-foo-bar',
+            handler='index.handler',
+            memory_size=128,
+            timeout=3,
+            role=ROLE,
+            runtime='python2.7',
+            description='test')
+        func_data.update(kw)
+
+        archive = PythonPackageArchive()
+        archive.add_contents('index.py',
+            '''def handler(*a, **kw):\n    print("Greetings, program!")''')
+        archive.close()
+        self.addCleanup(archive.remove)
+
+        return LambdaFunction(func_data, archive)
+
+
+    def test_publishes_a_lambda(self):
+        session_factory = self.replay_flight_data('test_publishes_a_lambda')
+        mgr = LambdaManager(session_factory)
+        func = self.make_func()
+        self.addCleanup(mgr.remove, func)
+        result = mgr.publish(func)
+        self.assertEqual(result['CodeSize'], 169)
+
+    def test_can_switch_runtimes(self):
+        session_factory = self.replay_flight_data('test_can_switch_runtimes')
+        func = self.make_func()
+        mgr = LambdaManager(session_factory)
+        self.addCleanup(mgr.remove, func)
+        result = mgr.publish(func)
+        self.assertEqual(result['Runtime'], 'python2.7')
+
+        func.func_data['runtime'] = 'python3.6'
+        result = mgr.publish(func)
+        self.assertEqual(result['Runtime'], 'python3.6')
 
 
 class PolicyLambdaProvision(BaseTest):
@@ -53,7 +99,7 @@ class PolicyLambdaProvision(BaseTest):
         }, Config.empty())
         pl = PolicyLambda(p)
         mgr = LambdaManager(session_factory)
-        result = mgr.publish(pl, 'Dev', role=self.role)
+        result = mgr.publish(pl, 'Dev', role=ROLE)
         self.assertEqual(result['FunctionName'], 'custodian-sg-modified')
         self.addCleanup(mgr.remove, pl)
 
@@ -85,7 +131,7 @@ class PolicyLambdaProvision(BaseTest):
         params = dict(
             session_factory=session_factory,
             name="c7n-log-sub",
-            role=self.role,
+            role=ROLE,
             sns_topic="arn:",
             log_groups=[linfo])
 
@@ -161,7 +207,7 @@ class PolicyLambdaProvision(BaseTest):
         }, Config.empty())
         pl = PolicyLambda(p)
         mgr = LambdaManager(session_factory)
-        result = mgr.publish(pl, 'Dev', role=self.role)
+        result = mgr.publish(pl, 'Dev', role=ROLE)
         self.addCleanup(mgr.remove, pl)
 
         p = Policy({
@@ -183,7 +229,7 @@ class PolicyLambdaProvision(BaseTest):
         }, Config.empty())
 
         output = self.capture_logging('custodian.lambda', level=logging.DEBUG)
-        result2 = mgr.publish(PolicyLambda(p), 'Dev', role=self.role)
+        result2 = mgr.publish(PolicyLambda(p), 'Dev', role=ROLE)
 
         lines = output.getvalue().strip().split('\n')
         self.assertTrue(
@@ -216,7 +262,7 @@ class PolicyLambdaProvision(BaseTest):
         pl = PolicyLambda(p)
         mgr = LambdaManager(session_factory)
         self.addCleanup(mgr.remove, pl)
-        result = mgr.publish(pl, 'Dev', role=self.role)
+        result = mgr.publish(pl, 'Dev', role=ROLE)
 
         events = pl.get_events(session_factory)
         self.assertEqual(len(events), 1)
@@ -269,7 +315,7 @@ class PolicyLambdaProvision(BaseTest):
         pl = PolicyLambda(p)
         mgr = LambdaManager(session_factory)
         self.addCleanup(mgr.remove, pl)
-        result = mgr.publish(pl, 'Dev', role=self.role)
+        result = mgr.publish(pl, 'Dev', role=ROLE)
         self.assert_items(
             result,
             {'Description': 'cloud-custodian lambda policy',
@@ -305,7 +351,7 @@ class PolicyLambdaProvision(BaseTest):
         pl = PolicyLambda(p)
         mgr = LambdaManager(session_factory)
         self.addCleanup(mgr.remove, pl)
-        result = mgr.publish(pl, 'Dev', role=self.role)
+        result = mgr.publish(pl, 'Dev', role=ROLE)
         self.assert_items(
             result,
             {'FunctionName': 'custodian-asg-spin-detector',
@@ -341,7 +387,7 @@ class PolicyLambdaProvision(BaseTest):
         pl = PolicyLambda(p)
         mgr = LambdaManager(session_factory)
         self.addCleanup(mgr.remove, pl)
-        result = mgr.publish(pl, 'Dev', role=self.role)
+        result = mgr.publish(pl, 'Dev', role=ROLE)
         self.assert_items(
             result,
             {'FunctionName': 'custodian-periodic-ec2-checker',
@@ -377,7 +423,11 @@ class PolicyLambdaProvision(BaseTest):
         }, Config.empty())
         pl = PolicyLambda(p)
         mgr = LambdaManager(session_factory)
-        self.addCleanup(mgr.remove, pl)
+        def cleanup():
+            mgr.remove(pl)
+            if self.recording:
+                time.sleep(60)
+        self.addCleanup(cleanup)
         return mgr, mgr.publish(pl)
 
     def create_a_lambda_with_lots_of_config(self, flight):
@@ -449,6 +499,7 @@ class PolicyLambdaProvision(BaseTest):
         mgr, result = self.create_a_lambda_with_lots_of_config(
             'test_config_coverage_for_lambda_update_from_complex')
         result = self.update_a_lambda(mgr, **{
+            'runtime': 'python3.6',
             'environment': {'Variables': {'FOO': 'baz'}},
             'kms_key_arn': '',
             'dead_letter_config': {},
@@ -461,7 +512,7 @@ class PolicyLambdaProvision(BaseTest):
              'FunctionName': 'custodian-hello-world',
              'Handler': 'custodian_policy.run',
              'MemorySize': 512,
-             'Runtime': 'python2.7',
+             'Runtime': 'python3.6',
              'Timeout': 60,
              'DeadLetterConfig': {'TargetArn': self.sns_arn},
              'Environment': {'Variables': {'FOO': 'baz'}},

tests/test_schema.py
@@ -285,3 +285,16 @@ class SchemaTest(BaseTest):
         errors = list(validator.iter_errors(data))
         self.assertEqual(len(errors), 0)
 
+    def test_runtime(self):
+        data = lambda runtime: {
+            'policies': [{
+                'name': 'test',
+                'resource': 's3',
+                'mode': {
+                    'type': 'periodic',
+                    'runtime': runtime}}]
+            }
+        errors_with = lambda r: list(Validator(generate()).iter_errors(data(r)))
+        self.assertEqual(len(errors_with('python2.7')), 0)
+        self.assertEqual(len(errors_with('python3.6')), 0)
+        self.assertEqual(len(errors_with('python4.5')), 1)

commit f1cf952b736c16df9a0576fa9df276853912fd29

Fixing execution-options not being allowed in schema.  Regression from PR #1638

c7n/schema.py
@@ -216,6 +216,22 @@ def generate(resource_types=()):
                 'kms_key_arn': {'type': 'string'},
                 'tracing_config': {'type': 'object'},
                 'tags': {'type': 'object'},
+                'execution-options': {
+                    'type': 'object',
+                    'additionalProperties': False,
+                    'properties': {
+                        'region': {'type': 'string'},
+                        'cache': {'type': 'string'},
+                        'profile': {'type': 'string'},
+                        'account_id': {'type': 'string'},
+                        'assume_role': {'type': 'string'},
+                        'log_group': {'type': 'string'},
+                        'metrics_enabled': {'type': 'boolean'},
+                        'output_dir': {'type': 'string'},
+                        'cache_period': {'type': 'number'},
+                        'dryrun': {'type': 'boolean'},
+                    }
+                }
             },
         },
     }