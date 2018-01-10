83c5f27bdee740162eb334d0c1712c5a364dc5a0
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2018-01-10 11:01:49 -0500

    split long line

--- tools/c7n_guardian/c7n_guardian/cli.py
+++ tools/c7n_guardian/c7n_guardian/cli.py
@@ -38,7 +38,8 @@ def cli():
 
 
 @cli.command()
-@click.option('-c', '--config', required=True, help="Accounts config file", type=click.Path())
+@click.option('-c', '--config',
+              required=True, help="Accounts config file", type=click.Path())
 @click.option('-t', '--tags', multiple=True, default=None)
 @click.option('-a', '--accounts', multiple=True, default=None)
 @click.option('--master', help='Master account id or name')
@@ -79,7 +80,8 @@ def report(config, tags, accounts, master, debug):
 
 
 @cli.command()
-@click.option('-c', '--config', required=True, help="Accounts config file", type=click.Path())
+@click.option('-c', '--config',
+              required=True, help="Accounts config file", type=click.Path())
 @click.option('-t', '--tags', multiple=True, default=None)
 @click.option('-a', '--accounts', multiple=True, default=None)
 @click.option('--master', help='Master account id or name')
