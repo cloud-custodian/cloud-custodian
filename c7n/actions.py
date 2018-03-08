b59041cf215b6727b52b81df52d1866d98257711
Matthew Gladney <matthew.gladney@capitalone.com>
2018-03-08 16:25:18 -0500

    Allow use of account_id and region in statement strings

--- c7n/actions.py
+++ c7n/actions.py
@@ -864,6 +864,15 @@ class ModifyPolicyBase(BaseAction):
         }
     )
 
+    def __init__(self, data=None, manager=None, log_dir=None):
+        config_args = {
+            'account_id': manager.config.account_id,
+            'region': manager.config.region
+        }
+        self.data = utils.format_string_values(data, **config_args)
+        self.manager = manager
+        self.log_dir = log_dir
+
     def add_statements(self, policy_statements):
         current = {s['Sid']: s for s in policy_statements}
         additional = {s['Sid']: s for s in self.data.get('add-statements', [])}
