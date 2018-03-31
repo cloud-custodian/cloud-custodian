8d967f60dec645d0dd2d0bdf84f5027c1628b71b
Ferran Grau Horta <ferran.grau@scmspain.com>
2018-04-01 00:47:41 +0200

    correct some minor mistakes

--- tools/c7n_mailer/c7n_mailer/datadog_delivery.py
+++ tools/c7n_mailer/c7n_mailer/datadog_delivery.py
@@ -11,27 +11,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .utils import (
-    format_struct, get_message_subject, get_resource_tag_targets,
-    get_rendered_jinja)
 
-from datetime import datetime
+import time
+
 from datadog import initialize
 from datadog import api
 
 
 class DataDogDelivery(object):
+    DATADOG_APPLICATION_KEY = 'datadog_application_key'
+    DATADOG_API_KEY = 'datadog_api_key'
+
     def __init__(self, config, session, logger):
         self.config      = config
         self.logger      = logger
         self.session     = session
 
         # Initialize datadog
-        options = {
-            'api_key': self.config['datadog_api_key'],
-            'app_key': self.config['datadog_aplication_key']
-        }
-        initialize(**options)
+        if self.config.get(self.DATADOG_API_KEY, False) and self.config.get(self.DATADOG_APPLICATION_KEY, False):
+            options = {
+                'api_key': self.config[self.DATADOG_API_KEY],
+                'app_key': self.config[self.DATADOG_APPLICATION_KEY]
+
+            }
+            initialize(**options)
 
     def get_datadog_message_packages(self, sqs_message):
         datadog_rendered_messages = []
@@ -39,9 +42,9 @@ class DataDogDelivery(object):
         for resource in sqs_message['resources']:
             metric = [
                 'event:{}'.format(sqs_message['event']),
-                'account_id'.format(sqs_message['account_id']),
-                'account'.format(sqs_message['account']),
-                'region'.format(sqs_message['region'])
+                'account_id:{}'.format(sqs_message['account_id']),
+                'account:{}'.format(sqs_message['account']),
+                'region:{}'.format(sqs_message['region'])
             ]
 
             metric.extend(['{key}:{value}'.format(key=key, value=resource[key]) for key in resource.keys()])
@@ -52,7 +55,7 @@ class DataDogDelivery(object):
 
     def deliver_datadog_messages(self, datadog_message_packages, sqs_message):
         metric_name = self.get_metric_name(sqs_message=sqs_message)
-        date_time = datetime.now()
+        date_time = time.time()
         metrics = []
         for message in datadog_message_packages:
             metrics.append({
@@ -75,6 +78,7 @@ class DataDogDelivery(object):
     @staticmethod
     def get_metric_value(sqs_message, message):
         metric_value = 1
+        metric_value_tag = 'default'
         for action in sqs_message['policy']['actions']:
             if action['type'] == 'notify' and action.get('metric_value_tag', None):
                 metric_value_tag = action['metric_value_tag']
