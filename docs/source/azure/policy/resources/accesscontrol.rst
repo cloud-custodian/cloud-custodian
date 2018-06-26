2f68bfcf8e00842bbc2025b9b5ad41ef714b1501
Erin Welch <erwelch@Erins-MBP.guest.corp.microsoft.com>
2018-06-26 16:48:40 -0700

    Adding more docs and filters/actions to access control.

---
+++ docs/source/azure/policy/resources/accesscontrol.rst
@@ -0,0 +1,109 @@
+.. _azure_access_control:
+
+Access Control
+==============
+
+Cloud custodian supports both azure role assignments and role definitions.
+
+Filters
+-------
+- Standard Value Filter (see :ref:`filters`)
+
+``role``
+  Filters Role Assignments based on name of Role Definition
+
+  .. c7n-schema:: RoleFilter
+      :module: c7n_azure.resources.access_control
+
+``resource-access``
+  Filter Role Assignments based on access to an azure resource
+
+  .. c7n-schema:: ResourceAccessFilter
+      :module: c7n_azure.resources.access_control
+
+
+Actions
+-------
+
+``delete``
+  Deletes role assignment
+
+Example Policies
+----------------
+
+Return role assignments with the Owner role.
+
+.. code-block:: yaml
+
+    policies:
+       - name: assignment-by-role
+         resource: azure.roleassignment
+         filters:
+            - type: role
+              key: properties.roleName
+              op: eq
+              value: Owner
+
+Return all assignments with the Owner role that have access to virtual machines. For the
+resource-access filter, the related resource can be any custodian supported azure resource other than
+azure.roleassignments or azure.roledefinitions.
+
+.. code-block:: yaml
+
+    policies:
+       - name: assignment-by-role-and-resource
+         resource: azure.roleassignment
+         filters:
+            - type: role
+              key: properties.roleName
+              op: eq
+              value: Owner
+            - type: resource-access
+              relatedResource: azure.vm
+
+Return assignments with the principal name custodian@example.com:
+
+.. code-block:: yaml
+
+     policies:
+       - name: assignment-by-principal-name
+         resource: azure.roleassignment
+         filters:
+            - type: value
+              key: principalName
+              op: eq
+              value: custodian@example.com
+
+Return role definitions that explicitly have the permission to read authorization objects (role
+assignments, role definitions, etc). If a role definition inherits permissions (e.g. by having * permissions)
+they are not returned in this filter.
+
+.. code-block:: yaml
+
+    policies:
+        - name: role-definition-permissions
+          description: |
+            Adds a tag to all virtual machines
+          resource: azure.roledefinition
+          filters:
+            - type: value
+              key: properties.permissions[0].actions
+              value: Microsoft.Authorization/*/read
+              op: contains
+
+Delete the assignment with principal name custodian@example.com. The permissions required to run the
+delete action requires delete permissions to Microsoft.Authorization. The built-in role with the necessary permissions
+is Owner.
+
+.. code-block:: yaml
+
+     policies:
+       - name: delete-assignment-by-principal-name
+         resource: azure.roleassignment
+         filters:
+            - type: value
+              key: principalName
+              op: eq
+              value: custodian@example.com
+         actions:
+            - type: delete
--- tools/c7n_azure/c7n_azure/resources/access_control.py
+++ tools/c7n_azure/c7n_azure/resources/access_control.py
@@ -13,14 +13,20 @@
 # limitations under the License.
 from azure.common.credentials import get_azure_cli_credentials
 from azure.graphrbac import GraphRbacManagementClient
-from c7n_azure.provider import resources
+from azure.graphrbac.models import GetObjectsParameters
+from c7n_azure.provider import resources, Azure
 from c7n_azure.query import QueryResourceManager
 from c7n_azure.session import Session
+from c7n_azure.utils import ResourceIdParser
+from c7n.filters import FilterValidationError
 
-from c7n.filters import ValueFilter
+from c7n.actions import BaseAction
+from c7n.handler import Config
+from c7n.filters import ValueFilter, Filter
 from c7n.filters.related import RelatedResourceFilter
 from c7n.utils import local_session
 from c7n.utils import type_schema
+from c7n.ctx import ExecutionContext
 
 
 @resources.register('roleassignment')
@@ -33,8 +39,9 @@ class RoleAssignment(QueryResourceManager):
         get_spec = ('role_assignments', 'get_by_id', None)
         id = 'id'
         default_report_fields = (
-            'display_name',
-            'user_principal_name',
+            'principalName',
+            'displayName',
+            'aadType',
             'name',
             'type',
             'properties.scope',
@@ -44,17 +51,35 @@ class RoleAssignment(QueryResourceManager):
     def augment(self, resources):
         s = local_session(Session)
         cred, sub_id = get_azure_cli_credentials(resource='https://graph.windows.net')
-        client = GraphRbacManagementClient(cred, s.tenant_id)
+        graph_client = GraphRbacManagementClient(cred, s.tenant_id)
+
+        object_ids = list(set(
+            resource['properties']['principalId'] for resource in resources
+            if resource['properties']['principalId']))
+
+        object_params = GetObjectsParameters(
+            include_directory_object_references=True,
+            object_ids=object_ids)
+
+        aad_objects = graph_client.objects.get_objects_by_object_ids(object_params)
+
+        principal_dics = {aad_object.object_id: aad_object for aad_object in aad_objects}
+
         for resource in resources:
-            try:
-                user = client.users.get(resource['properties']['principalId'])
-                resource['display_name'] = user.display_name
-                resource['user_principal_name'] = user.user_principal_name
-            except Exception as e:
-                print('exception')
-                print(e)
+            graph_resource = principal_dics[resource['properties']['principalId']]
+            resource['principalName'] = self.get_principal_name(graph_resource)
+            resource['displayName'] = graph_resource.display_name
+            resource['aadType'] = graph_resource.object_type
 
         return resources
+
+    @staticmethod
+    def get_principal_name(graph_object):
+        if graph_object.user_principal_name:
+            return graph_object.user_principal_name
+        elif graph_object.service_principal_names:
+            return graph_object.service_principal_names[0]
+        return graph_object.display_name or ''
 
 
 @resources.register('roledefinition')
@@ -64,7 +89,8 @@ class RoleDefinition(QueryResourceManager):
         s = Session()
         service = 'azure.mgmt.authorization'
         client = 'AuthorizationManagementClient'
-        enum_spec = ('role_definitions', 'list', {'scope': '/subscriptions/%s' % (s.subscription_id)})
+        enum_spec = ('role_definitions', 'list',
+                     {'scope': '/subscriptions/%s' % (s.subscription_id)})
         get_spec = ('role_definitions', 'get_by_id', None)
         type = 'roleDefinition'
         id = 'id'
@@ -78,8 +104,9 @@ class RoleDefinition(QueryResourceManager):
             'properties.permissions'
         )
 
+
 @RoleAssignment.filter_registry.register('role')
-class UserRole(RelatedResourceFilter):
+class RoleFilter(RelatedResourceFilter):
     """Filters role assignments based on role definitions
 
     :Example:
@@ -100,3 +127,85 @@ class UserRole(RelatedResourceFilter):
 
     RelatedResource = "c7n_azure.resources.access_control.RoleDefinition"
     RelatedIdsExpression = "properties.roleDefinitionId"
+
+
+@RoleAssignment.filter_registry.register('resource-access')
+class ResourceAccessFilter(Filter):
+    """Filters role assignments that have access to a certain
+    type of azure resource.
+
+    :Example:
+
+        .. code-block:: yaml
+
+            policies:
+               - name: assignments-by-azure-resource
+                 resource: azure.roleassignment
+                 filters:
+                    - type: resource-access
+                      relatedResource: azure.vm
+    """
+
+    schema = type_schema(
+        'resource-access',
+        relatedResource={'type': 'string'},
+        required=['relatedResource']
+    )
+
+    def __init__(self, data, manager=None):
+        super(ResourceAccessFilter, self).__init__(data, manager)
+        resource_type = self.data['relatedResource']
+        self.factory = Azure.resources.get(
+            resource_type.rsplit('.', 1)[-1])
+
+    def get_related(self):
+        ctx = ExecutionContext(local_session(Session), self.data, Config.empty())
+
+        manager = self.factory(ctx, self.data)
+        return manager.source.get_resources(None)
+
+    def process(self, resources, event=None):
+        related_resources = self.get_related()
+        s = Session()
+        client = s.client('azure.mgmt.authorization.AuthorizationManagementClient')
+        assignment_ids = []
+        for resource in related_resources:
+            results = client.role_assignments.list_for_resource(
+                ResourceIdParser.get_resource_group(resource['id']),
+                ResourceIdParser.get_namespace(resource['id']),
+                '',
+                ResourceIdParser.get_resource_type(resource['id']),
+                resource['name'])
+
+            assignment_ids.extend([r.serialize(True)['id'] for r in results])
+
+        assignment_ids = list(set(assignment_ids))
+        return [r for r in resources if r['id'] in assignment_ids]
+
+    def validate(self):
+        if self.factory is None:
+            raise FilterValidationError(
+                "The related resource is not a valid azure resource"
+            )
+        if (self.data['relatedResource'] == 'azure.roleassignment' or
+                self.data['relatedResource'] == 'azure.roledefinition'):
+            raise FilterValidationError(
+                "The related resource can not be role assignments or role definitions"
+            )
+
+
+@RoleAssignment.action_registry.register('delete')
+class DeleteAssignmentAction(BaseAction):
+
+    schema = type_schema('delete')
+
+    def __init__(self, data=None, manager=None, log_dir=None):
+        super(DeleteAssignmentAction, self).__init__(data, manager, log_dir)
+        self.client = self.manager.get_client()
+
+    def delete(self, assignment_scope, assignment_name):
+        self.client.role_assignments.delete(assignment_scope, assignment_name)
+
+    def process(self, assignments):
+        for assignment in assignments:
+            self.delete(assignment['properties']['scope'], assignment['name'])
