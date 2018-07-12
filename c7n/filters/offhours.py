7db37f26073e660c3100f527c9d8f56f8323bb93
Christian Costa <christiancosta@christian-costa.local>
2018-07-12 15:06:40 +1200

    Adding NZTD (New Zealand Daylight Time) to the list of supported timezones.

--- c7n/filters/offhours.py
+++ c7n/filters/offhours.py
@@ -307,6 +307,7 @@ class Time(Filter):
         'sgt': 'Asia/Singapore',
         'aet': 'Australia/Sydney',
         'brt': 'America/Sao_Paulo',
+        'nzdt': 'Pacific/Auckland',
         'utc': 'Etc/UTC',
     }
 
