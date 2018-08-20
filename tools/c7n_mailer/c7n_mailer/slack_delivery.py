e4e617cdb4174072f68b4c3fd6e7dde7c7838de7
Troy Larson <troylar@gmail.com>
2018-08-20 09:52:58 -0400

    Add support for multiple webhooks

--- tools/c7n_mailer/c7n_mailer/slack_delivery.py
+++ tools/c7n_mailer/c7n_mailer/slack_delivery.py
@@ -66,6 +66,11 @@ class SlackDelivery(object):
                             self.logger, 'slack_template', 'slack_default')
                 self.logger.debug(
                     "Generating messages for recipient list produced by resource owner resolution.")
+            elif target.startswith('https://hooks.slack.com/'):
+                slack_messages[target] = get_rendered_jinja(
+                    target, sqs_message,
+                    resource_list,
+                    self.logger, 'slack_template', 'slack_default')
             elif target.startswith('slack://webhook/#') and self.config.get('slack_webhook'):
                 webhook_target = self.config.get('slack_webhook')
                 slack_messages[webhook_target] = get_rendered_jinja(
@@ -100,7 +105,7 @@ class SlackDelivery(object):
                 sqs_message['policy']['resource'],
                 str(len(sqs_message['resources'])),
                 sqs_message['action'].get('slack_template', 'slack_default'),
-                json.loads(payload, strict=False)["channel"])
+                key)
             )
 
             self.send_slack_msg(key, payload)
