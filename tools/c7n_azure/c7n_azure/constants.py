b84fa52c28014d541f311532df6eb88301f52280
Andy Luong <anluong@microsoft.com>
2018-08-21 17:47:40 -0700

    Add some comment updates

--- tools/c7n_azure/c7n_azure/constants.py
+++ tools/c7n_azure/c7n_azure/constants.py
@@ -1,2 +1,3 @@
+# Docker image versions available at https://hub.docker.com/r/microsoft/azure-functions/
 CONST_DOCKER_VERSION = 'DOCKER|mcr.microsoft.com/azure-functions/python:latest'
 CONST_FUNCTIONS_EXT_VERSION = 'beta'
