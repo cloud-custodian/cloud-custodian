1bbc499d674217d59a086237b3e3efb641216a8a
Brent Clements <brent.clements@capitalone.com>
2018-09-25 11:34:27 -0400

    Re-included import statements I accidently removed

--- tools/c7n_azure/c7n_azure/entry.py
+++ tools/c7n_azure/c7n_azure/entry.py
@@ -40,8 +40,10 @@ import c7n_azure.resources.datalake_store
 import c7n_azure.resources.redis
 import c7n_azure.resources.vmss
 import c7n_azure.resources.batch
-import c7n_azure.resources.appserviceplan  # noqa: F401
+import c7n_azure.resources.subscription
+import c7n_azure.resources.policy_assignments
 import c7n_azure.resources.image
+import c7n_azure.resources.appserviceplan  # noqa: F401
 
 
 def initialize_azure():
