5890b0b7e4139e744f0625449fefa47f5640fe7b
Justin Hauer <justinhauer@gmail.com>
2018-11-02 07:54:43 -0500

    Resolve email delivery based off tags
    Commenting out slack check in is_email function which resolved email delivery based off of tags.

--- tools/c7n_mailer/c7n_mailer/utils_email.py
+++ tools/c7n_mailer/c7n_mailer/utils_email.py
@@ -5,9 +5,9 @@ logger = logging.getLogger('c7n_mailer.utils.email')
 
 
 def is_email(target):
-    if target.startswith('slack://'):
-        logger.debug("Slack payload, not an email.")
-        return False
+  #  if target.startswith('slack://'):
+  #      logger.debug("Slack payload, not an email.")
+  #      return False
     if parseaddr(target)[1] and '@' in target and '.' in target:
         return True
     else:
