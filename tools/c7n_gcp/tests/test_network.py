b3d0c6b825a3916740b78a7b5bb1b423c00ee039
Kapil Thangavelu <kapilt@gmail.com>
2018-11-04 06:30:58 -0500

    gcp.subnet - flow log and private api endpoint actions

---
+++ tools/c7n_gcp/tests/test_network.py
@@ -0,0 +1,51 @@
+
+from gcp_common import BaseTest
+
+
+class SubnetTest(BaseTest):
+
+    def test_subnet_set_flow(self):
+        project_id = 'cloud-custodian'
+        factory = self.replay_flight_data('subnet-set-flow', project_id=project_id)
+        p = self.load_policy({
+            'name': 'all-subnets',
+            'resource': 'gcp.subnet',
+            'filters': [
+                {"id": "4686700484947109325"},
+                {"enableFlowLogs": "empty"}],
+            'actions': ['set-flow-log']}, session_factory=factory)
+        resources = p.run()
+
+        self.assertEqual(len(resources), 1)
+        subnet = resources.pop()
+        self.assertEqual(subnet['enableFlowLogs'], False)
+
+        client = p.resource_manager.get_client()
+        result = client.execute_query(
+            'get', {'project': project_id,
+                    'region': 'us-central1',
+                    'subnetwork': subnet['name']})
+        self.assertEqual(result['enableFlowLogs'], True)
+
+    def test_subnet_set_private_api(self):
+        project_id = 'cloud-custodian'
+        factory = self.replay_flight_data('subnet-set-private-api', project_id=project_id)
+        p = self.load_policy({
+            'name': 'one-subnet',
+            'resource': 'gcp.subnet',
+            'filters': [
+                {"id": "4686700484947109325"},
+                {"privateIpGoogleAccess": False}],
+            'actions': ['set-private-api']}, session_factory=factory)
+        resources = p.run()
+
+        self.assertEqual(len(resources), 1)
+        subnet = resources.pop()
+        self.assertEqual(subnet['privateIpGoogleAccess'], False)
+
+        client = p.resource_manager.get_client()
+        result = client.execute_query(
+            'get', {'project': project_id,
+                    'region': 'us-central1',
+                    'subnetwork': subnet['name']})
+        self.assertEqual(result['privateIpGoogleAccess'], True)
