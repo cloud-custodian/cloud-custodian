cf7fbad4f49d0f10beda68804b56e359dc8ddbdb
Kapil Thangavelu <kapilt@gmail.com>
2018-11-28 14:25:35 -0800

    import hub

--- c7n/actions/__init__.py
+++ c7n/actions/__init__.py
@@ -20,3 +20,5 @@ from .metric import PutMetric
 from .network import ModifyVpcSecurityGroupsAction
 from .notify import BaseNotify, Notify
 from .policy import RemovePolicyBase, ModifyPolicyBase
+
+import securityhub
