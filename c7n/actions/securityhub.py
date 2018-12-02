3cfd81e6efe19c61e9106006d2a3460f61280f69
David Filiatrault <filiatra@amazon.com>
2018-12-02 08:55:20 +0000

    add iam-role support

--- c7n/actions/securityhub.py
+++ c7n/actions/securityhub.py
@@ -23,7 +23,7 @@ from c7n.utils import type_schema, local_session, chunks
 
 from c7n.resources.ec2 import EC2
 from c7n.resources.s3 import S3, get_region
-from c7n.resources.iam import User, UserAccessKey
+from c7n.resources.iam import User, UserAccessKey, Role
 from c7n.version import version
 
 
@@ -306,3 +306,26 @@ class UserFinding(PostFinding):
             if tags:
                 user["Tags"] = tags
             return user
+
+@Role.action_registry.register("post-finding")
+class RoleFinding(PostFinding):
+    def format_resource(self, r):
+        details = {
+            "RoleName": r["RoleName"],
+            "RoleId": r["RoleId"],
+            "CreateDate": r["CreateDate"].isoformat(),
+        }
+        if "c7n:MatchedFilters" in r:
+            details["c7n:MatchedFilters"] = json.dumps(r["c7n:MatchedFilters"])
+        if "Description" in r:
+            details["Description"] = r["Description"]
+        role = {
+            "Type": "Other",
+            "Id": r["Arn"],
+            "Region": self.manager.config.region,
+            "Details": {"Other": details},
+        }
+        tags = {t["Key"]: t["Value"] for t in r.get("Tags", [])}
+        if tags:
+            role["Tags"] = tags
+        return role
