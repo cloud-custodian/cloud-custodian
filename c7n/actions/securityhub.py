741d4a13405345f4e0a45986a749ca5e28589903
David Filiatrault <filiatra@amazon.com>
2018-12-20 09:28:15 +0000

    enable cost optimization and updates

--- c7n/actions/securityhub.py
+++ c7n/actions/securityhub.py
@@ -148,19 +148,73 @@ class PostFinding(BaseAction):
         },
     )
 
+    def get_finding_tag(self, resource):
+        finding_tag = None
+        tags = resource.get('Tags', [])
+
+        finding_key = '{}:{}'.format('c7n:FindingId',self.data.get('title', self.manager.ctx.policy.name))
+
+        for t in tags:
+            key = t['Key']
+            value = t['Value']
+            if key == finding_key:
+                finding_tag = value
+        return finding_tag
+
+    def group_resources(self, resources):
+        grouped_resources = {}
+        for r in resources:
+            finding_tag = self.get_finding_tag(r)
+            if not finding_tag:
+                finding_tag = 'New'
+
+            if not grouped_resources.get(finding_tag):
+                grouped_resources[finding_tag] = list()
+
+            grouped_resources[finding_tag].append(r)
+
+        return grouped_resources
+        
     def process(self, resources, event=None):
         region_name = self.data.get('region', self.manager.config.region)
         client = local_session(
             self.manager.session_factory).client("securityhub", region_name=region_name)
-        for resource_set in chunks(resources, 10):
-            finding = self.get_finding(resource_set)
-            client.batch_import_findings(Findings=[finding])
 
-    def get_finding(self, resources):
+        now = datetime.utcnow().replace(tzinfo=tzutc()).isoformat()
+
+        for key,grouped_resources in self.group_resources(resources).items():
+            for resource_set in chunks(grouped_resources, 10):
+                if key == 'New':
+                    finding_id = None
+                    created_at = now
+                    updated_at = now
+                else:
+                    finding_id, created_at = self.get_finding_tag(resource_set[0]).split(':',1)
+                    updated_at = now
+
+                finding = self.get_finding(resource_set, finding_id, created_at, updated_at)
+                self.log.debug(
+                    "finding=%s" % (finding))
+
+                import_response = client.batch_import_findings(Findings=[finding])
+                self.log.info(
+                    "import_response=%s" % (import_response))
+
+    def get_finding(self, resources, existing_finding_id, created_at, updated_at):
         policy = self.manager.ctx.policy
         model = self.manager.resource_type
 
-        now = datetime.utcnow().replace(tzinfo=tzutc()).isoformat()
+        if existing_finding_id:
+            finding_id = existing_finding_id
+        else:
+            finding_id = '{}/{}/{}/{}'.format(
+                self.manager.config.region,
+                self.manager.config.account_id,
+                hashlib.md5(json.dumps(
+                    policy.data).encode('utf8')).hexdigest(),
+                hashlib.md5(json.dumps(list(sorted(
+                    [r[model.id] for r in resources]))).encode(
+                        'utf8')).hexdigest())
         finding = {
             "SchemaVersion": self.FindingVersion,
             "ProductArn": "arn:aws:securityhub:{}:{}:product/{}/{}".format(
@@ -174,19 +228,10 @@ class PostFinding(BaseAction):
                 "description", policy.data.get("description", "")
             ).strip(),
             "Title": self.data.get("title", policy.name),
-            "Id": "{}/{}/{}/{}".format(
-                self.manager.config.region,
-                self.manager.config.account_id,
-                hashlib.md5(json.dumps(policy.data).encode("utf8")).hexdigest(),
-                hashlib.md5(
-                    json.dumps(list(sorted([r[model.id] for r in resources]))).encode(
-                        "utf8"
-                    )
-                ).hexdigest(),
-            ),
+            'Id': finding_id,
             "GeneratorId": policy.name,
-            "CreatedAt": now,
-            "UpdatedAt": now,
+            'CreatedAt': created_at,
+            'UpdatedAt': updated_at,
             "RecordState": "ACTIVE",
         }
 
@@ -236,6 +281,11 @@ class PostFinding(BaseAction):
             finding_resources.append(self.format_resource(r))
         finding["Resources"] = finding_resources
         finding["Types"] = list(self.data["types"])
+        
+        if existing_finding_id:
+            tag_action = self.manager.action_registry.get('tag')
+            tag_action({'key': '{}:{}'.format('c7n:FindingId', self.data.get('title', policy.name)), 'value': '{}:{}'.format(finding_id,created_at)}, self.manager).process(resources)
+
         return filter_empty(finding)
 
     def format_resource(self, r):
