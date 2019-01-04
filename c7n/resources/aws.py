8f133a1972f60492cefe6b7b07a5c745b169bda9
Kapil Thangavelu <kapilt@gmail.com>
2019-01-04 05:27:47 -0500

    fix cli usage of optional xray support, increment version, fix build pipeline link

--- c7n/resources/aws.py
+++ c7n/resources/aws.py
@@ -395,7 +395,7 @@ class AWS(object):
         """
         _default_region(options)
         _default_account_id(options)
-        if options.tracer:
+        if 'xray' in options.tracer and HAVE_XRAY:
             XrayTracer.initialize()
 
         return options
