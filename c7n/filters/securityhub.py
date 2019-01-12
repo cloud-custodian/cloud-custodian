f5fdb5c7be2eb86b273d106ae630b0d1e54de20a
David Filiatrault <filiatra@amazon.com>
2019-01-12 16:17:50 +0000

    fix lint

--- c7n/filters/securityhub.py
+++ c7n/filters/securityhub.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from c7n.utils import local_session, chunks, type_schema
+from c7n.utils import local_session, type_schema
 from .core import Filter
 from c7n.manager import resources
 
