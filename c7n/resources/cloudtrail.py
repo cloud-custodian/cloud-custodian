95706f6225daa24201fe1ba3be4e97f9c92c0496
Kapil Thangavelu <kapilt@gmail.com>
2019-01-15 11:58:01 -0500

    cloudtrail filters & actions

--- c7n/resources/cloudtrail.py
+++ c7n/resources/cloudtrail.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2017 Capital One Services, LLC
+# Copyright 2017-2019 Capital One Services, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +13,13 @@
 # limitations under the License.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from c7n.actions import Action
+from c7n.filters import ValueFilter
 from c7n.manager import resources
 from c7n.query import QueryResourceManager
+from c7n.utils import local_session, type_schema
+
+from .aws import shape_validate
 
 
 @resources.register('cloudtrail')
@@ -32,3 +37,118 @@ class CloudTrail(QueryResourceManager):
         name = 'Name'
         dimension = None
         config_type = "AWS::CloudTrail::Trail"
+
+
+@CloudTrail.filter_registry.register('status')
+class Status(ValueFilter):
+    """Filter a cloudtrail by its status.
+
+    :Example:
+
+    .. code-block:: yaml
+
+        policies:
+          - name: cloudtrail-not-active
+            resource: aws.cloudtrail
+            filters:
+            - type: status
+              key: IsLogging
+              value: False
+    """
+
+    schema = type('status', rinherit=ValueFilter.schema)
+    permissions = ('cloudtrail:GetTrailStatus',)
+    annotation_key = 'c7n:TrailStatus'
+
+    def process(self, resources):
+        client = local_session(
+            self.manager.session_factory).client('cloudtrail')
+        for r in resources:
+            if self.annotation_key in r:
+                continue
+            r[self.annotation_key] = client.get_trail_status(
+                Name=r['Name'])
+        return super(Status, self).process(resources)
+
+    def __call__(self, r):
+        return self.match(r['c7n:TrailStatus'])
+
+
+@CloudTrail.action_registry.register('update-trail')
+class UpdateTrail(Action):
+    """Update trail attributes.
+
+    :Example:
+
+    .. code-block:: yaml
+
+       policies:
+         - name: cloudtrail-set-log
+           resource: aws.cloudtrail
+           filters:
+            - or:
+              - KmsKeyId: empty
+              - LogFileValidationEnabled: false
+           actions:
+            - type: update-trail
+              attribute:
+                KmsKeyId: arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef
+                LogFileValidationEnabled: true
+    """
+    schema = type_schema(
+        'update-trail',
+        attributes={'type': 'object'},
+        required=('attributes',))
+    shape = 'UpdateTrailRequest'
+
+    def validate(self):
+        return shape_validate(
+            self.data['attributes'],
+            self.shape,
+            self.manager.resource_type.service)
+
+    def process(self, resources):
+        client = local_session(self.manager.session_factory)
+        for r in resources:
+            client.update_trail(
+                Name=r['Name'],
+                **self.data.attributes)
+
+
+@CloudTrail.action_registry.register('set-logging')
+class SetLogging(Action):
+    """Set the logging state of a trail
+
+    :Example:
+
+    .. code-block:: yaml
+
+      policies:
+        - name: cloudtrail-not-active
+          resource: aws.cloudtrail
+          filters:
+           - type: status
+             key: IsLogging
+             value: False
+          actions:
+           - type: set-logging
+             enabled: True
+    """
+    schema = type_schema(
+        'set-logging', {'enabled': {'type': 'boolean'}})
+
+    def get_permissions(self):
+        if self.data.get('state', True) == True:
+            return ('cloudtrail:StartLogging',)
+        else:
+            return ('cloudtrail:StopLogging',)
+
+    def process(self, resources):
+        client = local_session(
+            self.manager.session_factory).client('cloudtrail')
+        enable = self.data.get('state', True)
+        for r in resources:
+            if enable:
+                client.start_logging(Name=r['Name'])
+            else:
+                client.stop_logging(Name=r['Name'])
