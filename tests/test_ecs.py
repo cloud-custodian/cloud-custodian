8cdd826730cc3e1148cf0d270af0a6aa4dcecdf8
Mohr <robert.mohr@capitalone.com>
2019-02-22 14:58:09 -0500

    rerun service tests for smaller placebo data

--- tests/test_ecs.py
+++ tests/test_ecs.py
@@ -147,7 +147,7 @@ class TestEcsService(BaseTest):
             session_factory=session_factory,
         )
         resources = p.run()
-        self.assertEqual(len(resources), 2)
+        self.assertEqual(len(resources), 1)
         self.assertEqual(resources[0]["serviceName"], "home-web")
 
     def test_ecs_service_tag(self):
