e238b6dac33e40d5fc730f3a602b617dc75e06e8
claque2000 <dev@claque2000.net>
2019-03-08 23:04:38 +0100

    Add base bucket filter class for reusability, interpolation for has-statement filter and amend tests for this feature

--- c7n/resources/s3.py
+++ c7n/resources/s3.py
@@ -730,6 +730,14 @@ class BucketActionBase(BaseAction):
                 results += filter(None, [f.result()])
             return results
 
+class BucketFilterBase(Filter):
+    def get_std_format_args(self, bucket):
+        return {
+            'account_id': self.manager.config.account_id,
+            'region': self.manager.config.region,
+            'bucket_name': bucket['Name'],
+            'bucket_region': get_region(bucket)
+        }
 
 @S3.action_registry.register("post-finding")
 class BucketFinding(PostFinding):
@@ -750,7 +758,7 @@ class BucketFinding(PostFinding):
 
 
 @filters.register('has-statement')
-class HasStatementFilter(Filter):
+class HasStatementFilter(BucketFilterBase):
     """Find buckets with set of policy statements.
 
     :example:
@@ -820,7 +828,8 @@ class HasStatementFilter(Filter):
             if s.get('Sid') in required:
                 required.remove(s['Sid'])
 
-        required_statements = list(self.data.get('statements', []))
+        required_statements = format_string_values(list(self.data.get('statements', [])),
+                                                   **self.get_std_format_args(b))
         for required_statement in required_statements:
             for statement in statements:
                 found = 0
