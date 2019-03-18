c93d70e28029369dc4c8f8a5b38109237f40cdea
Kapil Thangavelu <kapilt@gmail.com>
2019-03-18 16:00:16 -0400

    gcp resource metadata include id field

--- tools/c7n_gcp/c7n_gcp/resources/compute.py
+++ tools/c7n_gcp/c7n_gcp/resources/compute.py
@@ -32,6 +32,7 @@ class Instance(QueryResourceManager):
         component = 'instances'
         enum_spec = ('aggregatedList', 'items.*.instances[]', None)
         scope = 'project'
+        id = 'name'
 
         @staticmethod
         def get(client, resource_info):
@@ -119,6 +120,7 @@ class Disk(QueryResourceManager):
         component = 'disks'
         scope = 'zone'
         enum_spec = ('aggregatedList', 'items.*.disks[]', None)
+        id = 'name'
 
         @staticmethod
         def get(client, resource_info):
@@ -146,6 +148,7 @@ class Snapshot(QueryResourceManager):
         version = 'v1'
         component = 'snapshots'
         enum_spec = ('list', 'items[]', None)
+        id = 'name'
 
         @staticmethod
         def get(client, resource_info):
