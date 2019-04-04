bdd2f783d206cee802862b5d56466328c52b5faa
anovis <anovis@ymail.com>
2019-04-04 15:00:25 -0400

    parameterized doc tests

--- tests/test_doc_examples.py
+++ tests/test_doc_examples.py
@@ -13,15 +13,15 @@
 # limitations under the License.
 import sys
 import yaml
-import pytest
+from parameterized import parameterized
 
 from c7n.resources import load_resources
+from c7n.exceptions import PolicyValidationError
 from .common import BaseTest
 
 
 def get_doc_examples():
     policies = []
-    names = []
     load_resources()
     for mod in sys.modules.keys():
         if 'c7n.resources' in mod:
@@ -30,28 +30,32 @@ def get_doc_examples():
                 cls = getattr(sys.modules[mod], sub_item, None)
                 if isinstance(cls, type):
                     if cls.__doc__:
-                        splt_doc = cls.__doc__.split('yaml\n\n            ') # fix reget to catch all tests
+                        splt_doc = cls.__doc__.split('yaml') # fix reget to catch all tests
                         if len(splt_doc) == 2:
                                 # pp = yaml.load(splt_doc[1])
                                 # p = self.load_policy(pp["policies"][0])
-                            policies.append((splt_doc[1],))
-                            names.append(cls.__name__)
+                            policies.append((splt_doc[1], module.__name__,cls.__name__))
 
-    return policies, names
+    return policies
 
+
+def custom_test_name(testcase_func, param_num, param):
+    return "test_docs_%s_%s" % (
+        param.args[1],
+        param.args[2]
+
+    )
 
 
 class DocExampleTest(BaseTest):
 
-    policies_in_docs, names = get_doc_examples()
-    policies_in_docs = ["x"]
+    policies_in_docs = get_doc_examples()
 
-    @pytest.mark.parametrize(['policy'], policies_in_docs,  scope="class")
-    def test_doc_examples(self, policy):
-        pol = yaml.load(policy)
-        self.load_policy(pol)
+    @parameterized.expand(policies_in_docs, name_func=custom_test_name)
+    def test_doc_examples(self, policy, module, name):
         try:
-            assert pol.validate()
-            # print(p)
-        except:
-            assert 0
+            parsed_policy = yaml.load(policy)
+            policy = self.load_policy(parsed_policy["policies"][0])
+            self.assertIsNone(policy.validate())
+        except PolicyValidationError:
+            self.assertIsNone(None)
