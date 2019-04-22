5066b0fd598b5026723143bf139135cf30444751
Yauhen Shaliou <yauhen_shaliou@epam.com>
2019-04-22 18:08:51 +0300

    [Cloud Spanner] Databases: initial setup

--- tools/c7n_gcp/tests/test_spanner.py
+++ tools/c7n_gcp/tests/test_spanner.py
@@ -33,19 +33,54 @@ class SpannerInstanceTest(BaseTest):
         self.assertEqual(resources[0]['displayName'], 'test-instance')
 
     def test_spanner_instance_get(self):
-        project_id = 'atomic-shine-231410'
+        project_id = 'custodiantestproject'
         session_factory = self.replay_flight_data('spanner-instance-get', project_id=project_id)
 
-        policy = {
-            'name': 'one-spanner-instance',
-            'resource': 'gcp.spanner-instance'
-        }
-
         policy = self.load_policy(
-            policy,
+            {'name': 'one-spanner-instance',
+             'resource': 'gcp.spanner-instance'},
             session_factory=session_factory)
 
         instance = policy.resource_manager.get_resource(
-            {'name': 'projects/atomic-shine-231410/instances/test-instance'})
+            {'resourceName': 'projects/custodiantestproject/instances/custodian-spanner'})
 
         self.assertEqual(instance['state'], 'READY')
+
+
+class SpannerDatabaseInstanceTest(BaseTest):
+
+    def test_spanner_database_instance_query(self):
+        project_id = 'custodiantestproject'
+        session_factory = self.replay_flight_data('spanner-database-instance-query',
+                                                  project_id=project_id)
+
+        policy = self.load_policy(
+            {'name': 'all-spanner-database-instances',
+             'resource': 'gcp.spanner-database-instance'},
+            session_factory=session_factory)
+
+        resources = policy.run()
+        self.assertEqual(resources[0]['c7n:spanner-instance']['displayName'], 'custodian-spanner')
+        self.assertEqual(resources[0]['c7n:spanner-instance']['state'], 'READY')
+        self.assertEqual(resources[0]['c7n:spanner-instance']['nodeCount'], 1)
+
+    def test_spanner_database_instance_get(self):
+        project_id = 'custodiantestproject'
+        session_factory = self.replay_flight_data('spanner-database-instance-get',
+                                                  project_id=project_id)
+
+        resource_name = 'projects/custodiantestproject/instances/spanner-instance' \
+                        '/databases/custodian-database'
+
+        policy = self.load_policy(
+            {'name': 'one-spanner-database-instance',
+             'resource': 'gcp.spanner-database-instance'},
+            session_factory=session_factory)
+
+        instance = policy.resource_manager.get_resource({'resourceName': resource_name})
+
+        self.assertEqual(instance['state'], 'READY')
+        self.assertEqual(instance['c7n:spanner-instance']['displayName'], 'spanner-instance')
+        self.assertEqual(instance['c7n:spanner-instance']['name'],
+                         'projects/custodiantestproject/instances/spanner-instance')
+        self.assertEqual(instance['name'], resource_name)
