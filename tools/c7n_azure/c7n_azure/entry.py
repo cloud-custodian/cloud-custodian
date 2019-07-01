d6c22fadb670d967d2168ce22406b4e95148fd11
Cameron Taylor <axis7818@gmail.com>
2019-07-01 14:38:29 -0700

    record set child resource of dns zone

--- tools/c7n_azure/c7n_azure/entry.py
+++ tools/c7n_azure/c7n_azure/entry.py
@@ -57,7 +57,8 @@ import c7n_azure.resources.event_subscription
 import c7n_azure.resources.k8s_service
 import c7n_azure.resources.apimanagement
 import c7n_azure.resources.appserviceplan
-import c7n_azure.resources.dnszone  # noqa: F401
+import c7n_azure.resources.dns_zone
+import c7n_azure.resources.record_set  # noqa: F401
 
 
 def initialize_azure():
