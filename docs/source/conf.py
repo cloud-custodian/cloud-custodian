9083b834e8aa8b5a024e2214b6c519291caa8d18
Kirill Logachev <kiril@microsoft.com>
2019-07-09 13:15:47 -0500

    Update style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,7 +126,6 @@ html_theme_options = {
   # Toc options
   'collapse_navigation': False,
   'sticky_navigation': True,
-  'navigation_depth': 8,
   'includehidden': True,
   'titles_only': False
 }
