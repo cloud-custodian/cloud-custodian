9d5e4f77e0d3710f70b3493483a4a253898d0765
Kirill Logachev <logachev.k@gmail.com>
2019-09-13 09:47:59 -0700

    Update storage_container.py

--- tools/c7n_azure/c7n_azure/resources/storage_container.py
+++ tools/c7n_azure/c7n_azure/resources/storage_container.py
@@ -51,7 +51,7 @@ class StorageContainer(ChildResourceManager):
         resource_type = 'Microsoft.Storage/storageAccounts/blobServices/containers'
         enable_tag_operations = False
         raise_on_exception = False
-        
+
         @classmethod
         def extra_args(cls, parent_resource):
             return {'resource_group_name': parent_resource['resourceGroup'],
