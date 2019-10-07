af5e01b55e02ce59e3a84c3c9cdd0add795f8a64
Cameron Taylor <axis7818@gmail.com>
2019-10-07 15:44:29 -0700

    cleanup and policy run validation

--- tools/c7n_azure/tests_azure/container-host/test_container_host.sh
+++ tools/c7n_azure/tests_azure/container-host/test_container_host.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 export AZURE_CORE_OUTPUT="none"
 
+resource_group_name="custodian-container-host-nightly"
+rg_tests=("aci-periodic" "aci-event" "aks-periodic" "aks-event")
+
 function cleanup {
-    set +ex
-    # TODO: clean up
+    set +e
+    echo "################################### Cleaning Up ###################################################"
+    echo "Deleting all test resources"
+    for rg_test in "${rg_tests[@]}"; do
+        az group delete --name "container-host-nightly-$rg_test" --yes
+    done
+    az group delete --name $resource_group_name --yes
     unset AZURE_CORE_OUTPUT
 }
 trap cleanup EXIT
@@ -14,12 +22,10 @@ echo "Logging in to Azure"
 az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
 az account set --subscription $AZURE_SUBSCRIPTION_ID
 
-##################################### Shared Infrastructure ########################################
 echo "################################## Creating Shared Resources #######################################"
 
 # Resource Group
 echo "Creating Resource Group"
-resource_group_name="custodian-container-host-nightly"
 location="westus2"
 az group create --name $resource_group_name --location $location
 
@@ -37,7 +43,6 @@ az extension add --name application-insights
 az monitor app-insights component create --resource-group $resource_group_name --app $app_insights_name --location $location
 instrumentation_key="azure://$(az monitor app-insights component show --resource-group $resource_group_name --app $app_insights_name --query "instrumentationKey" --output tsv)"
 
-############################################ ACI ###################################################
 echo "################################## Creating ACI Resources ##########################################"
 
 aci_name="custodian-aci"
@@ -49,7 +54,7 @@ aci_log_container_name="logs-aci"
 echo "Creating a Managed Identity"
 uai_name="custodian-aci"
 az identity create --resource-group $resource_group_name --name $uai_name
-sleep 60  # Give some time for the identity to propagate
+sleep 120  # Give some time for the identity to propagate
 
 # Role Assignments
 echo "Assigning Roles to the Managed Identity"
@@ -78,7 +83,6 @@ az group deployment create --resource-group $resource_group_name --template-file
     azure_container_metrics=$instrumentation_key \
     azure_container_output_dir="${storage_blob_endpoint/#https/azure}$aci_log_container_name"
 
-############################################ AKS ###################################################
 echo "################################## Creating AKS Resources ##########################################"
 
 aks_name="custodian-aks"
@@ -118,7 +122,6 @@ defaultEnvironment:
   AZURE_CONTAINER_METRICS: "$instrumentation_key"
   AZURE_CONTAINER_LOG_GROUP: "$instrumentation_key"
   AZURE_CONTAINER_OUTPUT_DIR: "${storage_blob_endpoint/#https/azure}$aks_log_container_name"
-
 subscriptionHosts:
   - name: "container-host"
     environment:
@@ -130,20 +133,17 @@ helm upgrade --install --wait --namespace $aks_name \
     $aks_name ../../../ops/azure/container-host/chart > /dev/null
 rm -f helm-values.yaml
 
-################################## Trigger Policies ################################################
 echo "################################## Triggering Policies ################################################"
+
+sleep 300
 
 # Create a resource group for each test policy
 echo "Creating Resource Groups Subjects for Policies"
-rg_tests=("aci-periodic" "aci-event" "aks-periodic" "aks-event")
 declare -A rg_test_results
 for rg_test in "${rg_tests[@]}"; do
     az group create --location $location --name "container-host-nightly-$rg_test"
     rg_test_results[$rg_test]="pending"
 done
-
-echo "Press 'enter' to start validating"
-read
 
 # Check that each policy has run
 echo "Verifying Tags from Policy Runs"
@@ -151,36 +151,38 @@ all_passed=false
 max_attempts=60
 for i in $(seq 1 $max_attempts); do 
     sleep 30
-    echo "  attempt $i of $max_attempts"
+    echo "Attempt $i of $max_attempts"
     for rg_test in "${rg_tests[@]}"; do
-        rg_test_results[$rg_test]=$(az group show --name "container-host-nightly-$rg_test" --query "tags.\"c7n-$rg_test\"")
+        rg_test_results["$rg_test"]=$(az group show --name "container-host-nightly-$rg_test" --query "tags.\"c7n-$rg_test\"" --output tsv)
     done
 
     all_passed=true
-    for result in "${rg_test_results[@]}"; do
-        if [[ $result -ne "passed" ]]; then all_passed=false; fi
+    for rg_test in "${rg_tests[@]}"; do
+        if [[ "passed" != ${rg_test_results["$rg_test"]} ]]; then 
+            all_passed=false 
+        fi
     done
-    if [[ all_passed -eq true ]]; then break; fi
-    echo "Trying again in 30 seconds..."
+    if [[ $all_passed == true ]]; then 
+        break
+    fi
+    echo "  trying again in 30 seconds..."
 done
 
-if [[ all_passed -ne true ]]; then
+if [[ $all_passed != true ]]; then
     echo "Some policies failed to run"
     for rg_test in "${rg_tests[@]}"; do
-        echo "  $rg_test: ${rg_test_results[$rg_test]}"
+        echo "  $rg_test: ${rg_test_results["$rg_test"]}"
     done
     exit 1
 fi
 echo "All policies ran"
 
-################################## Check Storage Output ############################################
-echo "################################# Checking Storage Output ##########################################"
-
-aci_output=$(az blob list --account-name $storage_account_name --account-key $storage_account_key --container-name $aci_log_container_name --query "[].name" --output tsv)
-aks_output=$(az blob list --account-name $storage_account_name --account-key $storage_account_key --container-name $aks_log_container_name --query "[].name" --output tsv)
+echo "Checking for Blob Storage Output"
+aci_output=$(az storage blob list --account-name $storage_account_name --account-key $storage_account_key --container-name $aci_log_container_name --query "[].name" --output tsv)
+aks_output=$(az storage blob list --account-name $storage_account_name --account-key $storage_account_key --container-name $aks_log_container_name --query "[].name" --output tsv)
 output_blobs="${aci_output[@]} ${aks_output[@]}"
 
-for rg_test in $rg_tests; do
+for rg_test in "${rg_tests[@]}"; do
     echo "Checking for $rg_test output"
     entries=$(grep -o "c7n-$rg_test" <<< "${output_blobs[@]}" | wc -l)
     if [[ $entries -eq 0 ]]; then
@@ -189,28 +191,26 @@ for rg_test in $rg_tests; do
     fi
 done
 
-####################################### Check Metrics And Logs #####################################
-echo "##################################### Checking Metrics And Logs ####################################"
-
-logs_query="traces | extend policy = tostring(customDimensions[\"Policy\"]) | project policy, message | where message contains \"count:1\""
+echo "Checking Logging and Monitoring in App Insights"
+logs_query='traces | extend policy = tostring(customDimensions["Policy"]) | project policy, message | where message contains "count:1"'
 policy_logs=$(az monitor app-insights query --resource-group $resource_group_name --app $app_insights_name \
-    --analytics-query $logs_query --offset 24h --query 'tables[0].rows' --output tsv)
+    --analytics-query "$logs_query" --offset 24h --query 'tables[0].rows' --output tsv)
 
-metrics_query="customMetrics | where name == \"ResourceCount\" and value == 1 | extend policy = tostring(customDimensions[\"Policy\"]) | project policy, value"
-resource_counts=$(az monitor app-insights query --app $app_insights_name \
-    --analytics-query $metrics_query --offset 24h --query 'tables[0].rows' --output tsv)
+metrics_query='customMetrics | where name == "ResourceCount" and value == 1 | extend policy = tostring(customDimensions["Policy"]) | project policy, value'
+resource_counts=$(az monitor app-insights query --resource-group $resource_group_name --app $app_insights_name \
+    --analytics-query "$metrics_query" --offset 24h --query 'tables[0].rows' --output tsv)
 
-for rg_test in $rg_tests; do
+for rg_test in "${rg_tests[@]}"; do
     log_line=$(grep c7n-$rg_test <<< $policy_logs | wc -l)
-    if [[ $log_line -ne 1 ]]; then
+    if [[ $log_line -eq 0 ]]; then
         echo "Failed to find log line for $rg_test"
         exit 1
     fi
     resource_count_line=$(grep c7n-$rg_test <<< $resource_counts | wc -l)
-    if [[ $resource_count_line -ne 1 ]]; then
+    if [[ $resource_count_line -eq 0 ]]; then
         echo "Failed to find resource count for $rg_test"
         exit 1
     fi
 done
 
-echo "Done!"
+echo "Test completed successfully"
