3c6004e0fc1675d1da739bd3f4b8d7bfa72e314d
Pratyush Mishra <Pratyush.Mishra@capitalone.com>
2020-01-27 11:01:30 -0500

    Conflicts

--- tests/test_efs.py
+++ tests/test_efs.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from __future__ import absolute_import, division, print_function, unicode_literals
-
-from .common import BaseTest, functional
 from c7n.exceptions import PolicyValidationError
+from .common import BaseTest, functional
+
 import uuid
 import time
 
