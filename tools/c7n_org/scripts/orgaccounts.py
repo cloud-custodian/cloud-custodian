0de7cc9d8f144caa5e30a175fd779e90c73c4cd7
Giulio Denardi <giulio.lpdenardi@gmail.com>
2020-03-02 16:28:40 -0300

    feat: add list of account exceptions

--- tools/c7n_org/scripts/orgaccounts.py
+++ tools/c7n_org/scripts/orgaccounts.py
@@ -37,7 +37,8 @@ ROLE_TEMPLATE = "arn:aws:iam::{Id}:role/OrganizationAccountAccessRole"
     '-f', '--output', type=click.File('w'),
     help="File to store the generated config (default stdout)")
 @click.option('-a', '--active', default=False, help="Get only active accounts", type=click.BOOL)
-def main(role, ou, assume, profile, output, regions, active):
+@click.option('-ex', '--exceptions', multiple=True, help="list of accounts that won't be added to the config file")
+def main(role, ou, assume, profile, output, regions, active, exceptions):
     """Generate a c7n-org accounts config file using AWS Organizations
 
     With c7n-org you can then run policies or arbitrary scripts across
@@ -49,7 +50,7 @@ def main(role, ou, assume, profile, output, regions, active):
     accounts = []
     for path in ou:
         ou = get_ou_from_path(client, path)
-        accounts.extend(get_accounts_for_ou(client, ou, active))
+        accounts.extend(get_accounts_for_ou(client, ou, active, exceptions=exceptions))
 
     results = []
     for a in accounts:
@@ -119,7 +120,7 @@ def get_sub_ous(client, ou):
     return results
 
 
-def get_accounts_for_ou(client, ou, active, recursive=True):
+def get_accounts_for_ou(client, ou, active, recursive=True, exceptions=[]):
     results = []
     ous = [ou]
     if recursive:
@@ -131,6 +132,10 @@ def get_accounts_for_ou(client, ou, active, recursive=True):
             ParentId=ou['Id']).build_full_result().get(
                 'Accounts', []):
             a['Path'] = ou['Path']
+
+            if a['Id'] in exceptions:
+              continue
+
             if active:
                 if a['Status'] == 'ACTIVE':
                     results.append(a)
