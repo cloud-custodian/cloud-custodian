e4bce801431d2abdf68e2b83347df02a74a46bfc
Pratyush Mishra <Pratyush.Mishra@capitalone.com>
2020-04-09 00:24:00 -0400

    Reverting dev endpoint subnet changes

--- c7n/resources/glue.py
+++ c7n/resources/glue.py
@@ -94,12 +94,6 @@ class GlueDevEndpoint(QueryResourceManager):
     augment = universal_augment
 
 
-@GlueDevEndpoint.filter_registry.register('subnet')
-class EndpointSubnetFilter(SubnetFilter):
-
-    RelatedIdsExpression = 'SubnetId'
-
-
 @GlueDevEndpoint.action_registry.register('delete')
 class DeleteDevEndpoint(BaseAction):
     """Deletes public Glue Dev Endpoints
