c534d8f08346f774409bd6d15f80e3c7658c6612
John Hillegass <hillegassdev@gmail.com>
2020-04-21 10:04:45 -0400

    fix doc string

--- c7n/resources/sagemaker.py
+++ c7n/resources/sagemaker.py
@@ -595,20 +595,22 @@ class NotebookKmsFilter(KmsRelatedFilter):
     of the kms key by using 'c7n:AliasName'
     :example:
         .. code-block:: yaml
+
             policies:
-                - name: sagemaker-kms-key-filters
-                  resource: aws.sagemaker-notebook
-                  filters:
-                    - type: kms-key
-                      key: c7n:AliasName
-                      value: "^(alias/aws/sagemaker)"
-                      op: regex
-                - name: sagemaker-endpoint-kms-key-filters
-                  resource: aws.sagemaker-endpoint-config
-                  filters:
-                    - type: kms-key
-                      key: c7n:AliasName
-                      value: "alias/aws/sagemaker"
+              - name: sagemaker-kms-key-filters
+                resource: aws.sagemaker-notebook
+                filters:
+                  - type: kms-key
+                    key: c7n:AliasName
+                    value: "^(alias/aws/sagemaker)"
+                    op: regex
+
+              - name: sagemaker-endpoint-kms-key-filters
+                resource: aws.sagemaker-endpoint-config
+                filters:
+                  - type: kms-key
+                    key: c7n:AliasName
+                    value: "alias/aws/sagemaker"
     """
     RelatedIdsExpression = "KmsKeyId"
 
