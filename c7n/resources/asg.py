c8d20db4175dee5ae3194d0cee764c7cd20beca7
Pratyush Mishra <Pratyush.Mishra@capitalone.com>
2020-05-11 18:19:59 -0400

    Check if asg has config

--- c7n/resources/asg.py
+++ c7n/resources/asg.py
@@ -574,9 +574,10 @@ class ImageAgeFilter(AgeFilter):
 
     def get_resource_date(self, asg):
         cfg = self.launch_info.get(asg)
-        ami = self.images.get(cfg.get('ImageId'), {})
-        return parse(ami.get(
-            self.date_attribute, "2000-01-01T01:01:01.000Z"))
+        if cfg:
+            ami = self.images.get(cfg.get('ImageId'), {})
+            return parse(ami.get(
+                self.date_attribute, "2000-01-01T01:01:01.000Z"))
 
 
 @ASG.filter_registry.register('image')
