e35d3612b5219cad901b5b9ef1ee98a1d0ccc0a9
Pratyush Mishra <Pratyush.Mishra@capitalone.com>
2020-05-12 08:49:10 -0400

    setting config to empty dict if None

--- c7n/resources/asg.py
+++ c7n/resources/asg.py
@@ -574,10 +574,11 @@ class ImageAgeFilter(AgeFilter):
 
     def get_resource_date(self, asg):
         cfg = self.launch_info.get(asg)
-        if cfg:
-            ami = self.images.get(cfg.get('ImageId'), {})
-            return parse(ami.get(
-                self.date_attribute, "2000-01-01T01:01:01.000Z"))
+        if cfg is None:
+            cfg = {}
+        ami = self.images.get(cfg.get('ImageId'), {})
+        return parse(ami.get(
+            self.date_attribute, "2000-01-01T01:01:01.000Z"))
 
 
 @ASG.filter_registry.register('image')
