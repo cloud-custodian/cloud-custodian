9207b3db445228b07c2cd2970811842c2051cbd5
Stefan Gordon <stefan@stefangordon.com>
2020-05-12 10:51:04 -0700

    Handle both failure modes, 2nd test

--- tools/c7n_azure/tests_azure/tests_resources/test_container_host.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_container_host.py
@@ -525,8 +525,8 @@ class ContainerHostTest(BaseTest):
                               - name: foo
                                 mode:
                                   type: container-periodic
-                                  schedule: '* * * *'
-                                resource: azure.resourcegroups
+                                  schedule: '* * * * *'
+                                resource: azure.resourcegroup
                         """
 
         # Create a bad yaml file
@@ -534,9 +534,38 @@ class ContainerHostTest(BaseTest):
         with open(file_path, 'w') as f:
             f.write(policy_string)
 
-        host.policies = []
+        host.unload_policy_file(file_path, {})
+        os_unlink.assert_called()
 
-        host.unload_policy_file(file_path, None)
+        # Clean up the file
+        os.remove(file_path)
+
+    @patch('c7n_azure.container_host.host.Host.update_event_subscription')
+    @patch('c7n_azure.container_host.host.BlockingScheduler.start')
+    @patch('c7n_azure.container_host.host.Host.prepare_queue_storage')
+    @patch('c7n_azure.container_host.host.Storage.get_queue_client_by_storage_account')
+    @patch('c7n_azure.container_host.host.Storage.get_blob_client_by_uri')
+    @patch('os.unlink')
+    def test_unload_policy_file_with_bad_schema(self,
+            os_unlink, _1, _2, _3, _4, _5):
+        host = Host(DEFAULT_EVENT_QUEUE_ID, DEFAULT_EVENT_QUEUE_NAME, DEFAULT_POLICY_STORAGE)
+
+        # Schedule is invalid
+        policy_string = """
+                            policie:
+                              - name: foo
+                                mode:
+                                  type: container-periodic
+                                  schedule: '* * * *'
+                                resource: azure.resourcegroup
+                        """
+
+        # Create a bad yaml file
+        file_path = tempfile.mktemp(suffix=".yaml")
+        with open(file_path, 'w') as f:
+            f.write(policy_string)
+
+        host.unload_policy_file(file_path, {})
         os_unlink.assert_called()
 
         # Clean up the file
