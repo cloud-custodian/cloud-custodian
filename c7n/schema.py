f88c32b818077bdd32b5cfdd23dd45861672d504
John Hillegass <hillegassdev@gmail.com>
2020-07-18 09:45:27 -0400

    core - policy - add metadata key

--- c7n/schema.py
+++ c7n/schema.py
@@ -261,6 +261,7 @@ def generate(resource_types=()):
                 'comments': {'type': 'string'},
                 'description': {'type': 'string'},
                 'tags': {'type': 'array', 'items': {'type': 'string'}},
+                'metadata': {'type': 'object'},
                 'mode': {'$ref': '#/definitions/policy-mode'},
                 'source': {'enum': ['describe', 'config', 'inventory',
                                     'resource-graph', 'disk', 'static']},
