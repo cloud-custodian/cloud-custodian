233b77ef45f64057a75ab31f3a7ee61ffcce6880
Pratyush Mishra <Pratyush.Mishra@capitalone.com>
2020-08-21 16:16:11 -0400

    CloudHsmCluster subnet filter and delete action

--- tests/test_hsm.py
+++ tests/test_hsm.py
@@ -22,3 +22,24 @@ class CloudHSMClusterTest(BaseTest):
         tags = client.list_tags(ResourceId=id)
         tag_map = {t["Key"]: t["Value"] for t in tags["TagList"]}
         self.assertTrue("foo" in tag_map)
+
+    def test_cloudhsm_subnet_delete(self):
+        factory = self.replay_flight_data("test_cloudhsm_subnet_delete")
+        client = factory().client("cloudhsmv2")
+        p = self.load_policy(
+            {
+                "name": "cloudhsm",
+                "resource": "cloudhsm-cluster",
+                "filters": [
+                    {"type": "subnet", "key": "SubnetId", "value": "subnet-0544e30702e2e1ad4"},
+                ],
+                "actions": [{"type": "delete"}]
+            },
+            session_factory=factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0].get('ClusterId'), 'cluster-p67ybp4edfr')
+        self.assertEqual(
+            client.describe_clusters(Filters={'clusterIds': ['cluster-p67ybp4edfe']}).get(
+                'Clusters'), [])
