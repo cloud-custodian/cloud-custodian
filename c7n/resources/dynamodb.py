470bc795c08e36b76de5716978da75c3908aaa44
David Filiatrault <filiatra@amazon.com>
2020-11-09 13:28:59 -0500

    fix lint

--- c7n/resources/dynamodb.py
+++ c7n/resources/dynamodb.py
@@ -22,8 +22,8 @@ class ConfigTable(query.ConfigSource):
     def load_resource(self, item):
         resource = super(ConfigTable, self).load_resource(item)
         resource['CreationDateTime'] = datetime.fromtimestamp(resource['CreationDateTime'] / 1000.0)
-        if 'BillingModeSummary' in resource and 
-            'LastUpdateToPayPerRequestDateTime' in resource['BillingModeSummary']:
+        if ('BillingModeSummary' in resource and
+            'LastUpdateToPayPerRequestDateTime' in resource['BillingModeSummary']):
             resource['BillingModeSummary'][
                 'LastUpdateToPayPerRequestDateTime'] = datetime.fromtimestamp(
                     resource['BillingModeSummary']['LastUpdateToPayPerRequestDateTime'] / 1000.0)
