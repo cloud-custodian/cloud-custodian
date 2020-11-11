0434ba00618b04fefbbc0cafb27923fffb8931b5
Kapil Thangavelu <kapilt@gmail.com>
2020-11-11 10:09:31 -0500

    fix shebang script line

--- tools/dev/citoxenv.py
+++ tools/dev/citoxenv.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
-#!/usr/bin/env python
+
 import os
 pyver = os.environ.get('TRAVIS_PYTHON_VERSION', '')
 if pyver == '2.7':
