56aabc73aa09b71dd81277c0c8e9cf46b873ef7c
v748275 <james.m.mwakichako@jpmchase.com>
2020-12-03 13:36:56 -0600

    Clean up print statement

--- c7n/resources/s3.py
+++ c7n/resources/s3.py
@@ -3117,7 +3117,6 @@ class KMSKeyResolverMixin:
                 if self.data.get('key').startswith('alias/aws'):
                     self.arns[r] = client.list_aliases(
                         KeyId=self.arns[r]).get('Aliases')[0].get('AliasArn')
-                    print("I am here")
             except ClientError as e:
                 self.log.error('Error resolving kms ARNs for set-bucket-encryption: %s key: %s' % (
                     e, self.data.get('key')))
