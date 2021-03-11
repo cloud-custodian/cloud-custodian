22c500eae5c5b6a3d66aa45391da384eefe4775b
John Hillegass <JohnHillegass@users.noreply.github.com>
2021-03-11 19:56:54 +0000

    work in progress

--- c7n/tags.py
+++ c7n/tags.py
@@ -10,6 +10,7 @@ snapshots).
 """
 from collections import Counter
 from concurrent.futures import as_completed
+from distutils.version import LooseVersion
 
 from datetime import datetime, timedelta
 from dateutil import tz as tzutil
@@ -17,6 +18,7 @@ from dateutil.parser import parse
 
 import jmespath
 import time
+import botocore
 
 from c7n.manager import resources as aws_resources
 from c7n.actions import BaseAction as Action, AutoTagUser
@@ -84,17 +86,24 @@ def universal_augment(self, resources):
     paginator.PAGE_ITERATOR_CLS = RetryPageIterator
 
     rfetch = [r for r in resources if 'Tags' not in r]
-
     for arn_resource_set in utils.chunks(
             zip(self.get_arns(rfetch), rfetch), 100):
         arn_resource_map = dict(arn_resource_set)
-        resource_tag_results = client.get_resources(
-            ResourceARNList=list(arn_resource_map.keys())).get(
-                'ResourceTagMappingList', ())
-        resource_tag_map = {
-            r['ResourceARN']: r['Tags'] for r in resource_tag_results}
-        for arn, r in arn_resource_map.items():
-            r['Tags'] = resource_tag_map.get(arn, [])
+
+    if LooseVersion(botocore.__version__) < LooseVersion('1.19.58'):
+        m = self.get_model()
+        resource_type = "%s:%s" % (m.arn_service or m.service, m.arn_type)        
+        resource_tag_results = list(itertools.chain(
+            *[p['ResourceTagMappingList'] for p in paginator.paginate(
+                ResourceTypeFilters=[resource_type])]))
+    else:   
+            resource_tag_results = client.get_resources(
+                ResourceARNList=list(arn_resource_map.keys())).get(
+                    'ResourceTagMappingList', ())
+    resource_tag_map = {
+        r['ResourceARN']: r['Tags'] for r in resource_tag_results}
+    for arn, r in arn_resource_map.items():
+        r['Tags'] = resource_tag_map.get(arn, [])
 
     return resources
 
