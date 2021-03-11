29a6c104034557d98dc62db1a19f368f28cc5dee
gswai123 <george.swain@utexas.edu>
2021-03-11 15:44:16 -0500

    Implement PR feedback

--- c7n/resources/mq.py
+++ c7n/resources/mq.py
@@ -8,17 +8,21 @@ from c7n.filters.kms import KmsRelatedFilter
 from c7n.manager import resources
 from c7n.query import QueryResourceManager, TypeInfo
 from c7n.utils import local_session, type_schema
-from c7n.tags import RemoveTag, Tag, TagDelayedAction, TagActionFilter, universal_augment
+from c7n.tags import (
+    RemoveTag,
+    Tag,
+    TagDelayedAction,
+    TagActionFilter,
+    universal_augment,
+)
 
 
 @resources.register('message-broker')
 class MessageBroker(QueryResourceManager):
-
     class resource_type(TypeInfo):
         service = 'mq'
         enum_spec = ('list_brokers', 'BrokerSummaries', None)
-        detail_spec = (
-            'describe_broker', 'BrokerId', 'BrokerId', None)
+        detail_spec = ('describe_broker', 'BrokerId', 'BrokerId', None)
         cfn_type = 'AWS::AmazonMQ::Broker'
         id = 'BrokerId'
         arn = 'BrokerArn'
@@ -54,6 +58,7 @@ class KmsFilter(KmsRelatedFilter):
                 value: "^(alias/aws/mq)"
                 op: regex
     """
+
     RelatedIdsExpression = 'EncryptionOptions.KmsKeyId'
 
 
@@ -77,58 +82,95 @@ class MQSGFilter(SecurityGroupFilter):
 
 @MessageBroker.filter_registry.register('metrics')
 class MQMetrics(MetricsFilter):
-
     def get_dimensions(self, resource):
         # Fetching for Active broker instance only, https://amzn.to/2tLBhEB
-        return [{'Name': self.model.dimension,
-                 'Value': "{}-1".format(resource['BrokerName'])}]
+        return [
+            {
+                'Name': self.model.dimension,
+                'Value': "{}-1".format(resource['BrokerName']),
+            }
+        ]
+
 
 @MessageBroker.filter_registry.register('vpc')
 class VpcFilter(VpcFilter):
+    """Filter a resource by its VPC id. Choose to select resources that are in that VPC or select
+    resources that are not in that VPC using the 'op' field. Security group ID is used to get
+    the VPC ID of a message broker, since DescribeMessageBrokers does not return a VPC ID.
+    Since message brokers are exclusively launched in a single VPC, and their security groups
+    all belong to this VPC, only the first security group id is needed to check the VPC
+    ID of the message broker. Continue is used in the function if a client error occurs
+    when trying to get a broker's vpc -- this will skip this broker, but still attempt to grab
+    the vpc ids of the other brokers in the list.
+
+    :example:
+
+    .. code-block:: yaml
+
+            policies:
+              - name: mq-vpc-filters
+                resource: message-broker
+                filters: [{'type': 'vpc', 'key': 'VpcId', 'value': 'vpc-xxxxxxxxxxx', 'op': 'eq'}]
+    """
 
     sg_vpc_dict = {}
 
     def get_related_ids(self, resources):
         client = local_session(self.manager.session_factory).client('ec2')
-        related_ids = []
+        related_ids = set()
         for r in resources:
-            sg_ids = r.get('SecurityGroups', [])
+            try:
+                sg_ids = r.get('SecurityGroups', [])
+            except IndexError:
+                continue
             vpc_id = self.sg_vpc_dict.get(sg_ids[0])
-            if(vpc_id):
-                related_ids.append(vpc_id)
+            if vpc_id:
+                related_ids.add(vpc_id)
                 continue
             try:
                 response = client.describe_security_groups(
                     GroupIds=[sg_ids[0]],
                 )
                 vpc_id = response.get('SecurityGroups')[0].get('VpcId')
-                related_ids.append(vpc_id)
+                related_ids.add(vpc_id)
                 self.sg_vpc_dict[sg_ids[0]] = vpc_id
-            except ClientError: 
-                continue 
-        return set(related_ids)
+            except ClientError:
+                continue
+        return related_ids
 
     RelatedIdsExpression = "VpcId"
 
 
 @MessageBroker.filter_registry.register('default-vpc')
 class DefaultVpc(DefaultVpcBase):
-    """ Matches if an mq broker is in the default vpc
+    """Matches if an mq broker is in the default vpc
+
+    :example:
+
+    .. code-block:: yaml
+
+            policies:
+              - name: mq-default-filters
+                resource: message-broker
+                filters: [{'type': 'default-vpc'}]
     """
 
     schema = type_schema('default-vpc')
 
     def retrieve_vpc_id(self, mq):
         client = local_session(self.manager.session_factory).client('ec2')
-        sg_ids = mq.get('SecurityGroups',[])
+        try:
+            sg_ids = mq.get('SecurityGroups', [])
+        except IndexError:
+            return
         try:
             response = client.describe_security_groups(
                 GroupIds=[sg_ids[0]],
             )
             vpc_id = response.get('SecurityGroups')[0].get('VpcId')
             return vpc_id
-        except IndexError: 
-            return 
+        except IndexError:
+            return
 
     def __call__(self, mq):
         vpc_id = self.retrieve_vpc_id(mq)
@@ -137,8 +179,7 @@ class DefaultVpc(DefaultVpcBase):
 
 @MessageBroker.action_registry.register('delete')
 class Delete(Action):
-    """Delete a set of message brokers
-    """
+    """Delete a set of message brokers"""
 
     schema = type_schema('delete')
     permissions = ("mq:DeleteBroker",)
@@ -178,7 +219,8 @@ class TagMessageBroker(Tag):
             try:
                 client.create_tags(
                     ResourceArn=r['BrokerArn'],
-                    Tags={t['Key']: t['Value'] for t in new_tags})
+                    Tags={t['Key']: t['Value'] for t in new_tags},
+                )
             except client.exceptions.ResourceNotFound:
                 continue
 
@@ -235,7 +277,6 @@ class MarkForOpMessageBroker(TagDelayedAction):
 
 @resources.register('message-config')
 class MessageConfig(QueryResourceManager):
-
     class resource_type(TypeInfo):
         service = 'mq'
         enum_spec = ('list_configurations', 'Configurations', None)
