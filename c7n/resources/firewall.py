8db9f41a786c2e74d1542bff321e6170e0f8190b
Kapil Thangavelu <kapilt@gmail.com>
2021-04-04 13:01:38 -0400

    config support / tags / subnet / vpc filters

--- c7n/resources/firewall.py
+++ c7n/resources/firewall.py
@@ -1,12 +1,10 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
 
-
+from .aws import AWS
 from c7n.query import (
-    QueryResourceManager,
-    TypeInfo,
-    DescribeSource,
-    ConfigSource)
+    QueryResourceManager, TypeInfo, DescribeSource, ConfigSource)
+from c7n.filters.vpc import VpcFilter, SubnetFilter
 
 
 class FirewallDescribe(DescribeSource):
@@ -15,16 +13,26 @@ class FirewallDescribe(DescribeSource):
         resources = super().augment(resources)
         for r in resources:
             status = r.pop('FirewallStatus', {})
+            r['Firewall']['UpdateToken'] = r['UpdateToken']
             r = r.pop('Firewall')
             r['FirewallStatus'] = status
         return resources
 
 
+class FirewallConfig(ConfigSource):
+
+    def load_resource(self, item):
+        resource = super().load_resource(item)
+        resource.update(resource.pop('Firewall'))
+        return resource
+
+
+@AWS.resources.register('firewall')
 class NetworkFirewall(QueryResourceManager):
 
     source_mapping = {
         'describe': FirewallDescribe,
-        'config': ConfigSource
+        'config': FirewallConfig
     }
 
     class resource_type(TypeInfo):
@@ -32,7 +40,21 @@ class NetworkFirewall(QueryResourceManager):
         service = 'network-firewall'
         enum_spec = ('list_firewalls', 'Firewalls', None)
         arn = 'FirewallArn'
+        arn_type = 'firewall'
         detail_spec = ('describe_firewall', 'FirewallArn', 'FirewallArn', '')
         id = name = 'FirewallName'
-        cfn_type = config_type = 'AWS::Network::Firewall'
+        cfn_type = config_type = 'AWS::NetworkFirewall::Firewall'
         metrics_namespace = 'AWS/NetworkFirewall'
+        universal_taggable = object()
+
+
+@NetworkFirewall.filter_registry.register('vpc')
+class FirewallVpcFilter(VpcFilter):
+
+    RelatedIdsExpression = 'VpcId'
+
+
+@NetworkFirewall.filter_registry.register('subnet')
+class FirewallSubnetFilter(SubnetFilter):
+
+    RelatedIdsExpression = 'SubnetMappings[].SubnetId'
