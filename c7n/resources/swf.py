00ead06ffa209e32baa48c3a432ad98aeeafdca5
kylejohnson514 <kylejohnson514@gmail.com>
2021-05-18 14:24:24 -0400

    Linting

--- c7n/resources/swf.py
+++ c7n/resources/swf.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 from c7n.manager import resources
 from c7n.query import ConfigSource, DescribeSource, QueryResourceManager, TypeInfo
-from c7n.tags import Tag, RemoveTag, universal_augment
+from c7n.tags import universal_augment
 
 
 class DescribeSimpleWorkflow(DescribeSource):
