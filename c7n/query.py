5b98950e04920a6299f46e205593f51ac196f6e4
Kapil Thangavelu <kapilt@gmail.com>
2021-07-26 10:52:43 -0400

    fix typo

--- c7n/query.py
+++ c7n/query.py
@@ -462,7 +462,7 @@ class QueryResourceManager(ResourceManager, metaclass=QueryMeta):
         if source_type in self.source_mapping:
             self.source_mapping.get(source_type)(self)
         if source_type in sources:
-            return souces[source_type](self)
+            return sources[source_type](self)
         raise KeyError("Invalid Source %s" % source_type)
 
     @classmethod
