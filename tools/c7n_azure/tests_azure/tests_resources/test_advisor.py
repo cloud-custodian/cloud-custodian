42dcc7459005b6a3ed03c6f6512b36b327a7ff87
lkakarla <lakshmipathi.k1987@gmail.com>
2021-08-13 13:38:09 +0000

    azure - renamed advisor (cloud-custodian#6836)

--- tools/c7n_azure/tests_azure/tests_resources/test_advisor.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_advisor.py
@@ -4,7 +4,7 @@ from ..azure_common import BaseTest
 
 
 class AdvisorRecommendationsTest(BaseTest):
-    def test_azure_advisor_schema_validate(self):
+    def test_azure_advisor_recommendations_schema_validate(self):
         p = self.load_policy({
             'name': 'test-azure-advisor-recommendations',
             'resource': 'azure.advisor-recommendations'
