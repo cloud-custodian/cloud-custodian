12e0e69433e626719f95c6bf11aa4c42c6ea05a2
Matthew Heidelbaugh <matthew.heidelbaugh@capitalone.com>
2021-09-22 15:40:26 -0400

    print permissions with action schema

--- c7n/commands.py
+++ c7n/commands.py
@@ -481,6 +481,8 @@ def schema_cmd(options):
     if len(components) == 3:
         cls = resource_info['classes'][category][item]
         _print_cls_schema(cls)
+        if category == 'actions':
+            print("Permissions: " + str(cls().get_permissions()))
 
         return
 
