b5c5cef24cee335a73e66f6b08bcc3e46b869b17
Kyle Johnson <kyle.johnson@premise.com>
2022-02-15 17:42:29 -0500

    fix

--- tools/c7n_gcp/c7n_gcp/filters/datacatalog.py
+++ tools/c7n_gcp/c7n_gcp/filters/datacatalog.py
@@ -43,7 +43,7 @@ class DataCatalogSearchFilter(Filter):
           resource: gcp.bq-table
           filters:
             - type: data-catalog
-              include_gcp_public_datasets: False
+              include_gcp_public_datasets: false
               include_org_ids:
                 - "112233445566"
               include_project_ids:
@@ -53,7 +53,7 @@ class DataCatalogSearchFilter(Filter):
     """
     schema = type_schema(
         'tag-template',
-        include_gcp_public_datasets={'type': 'boolean'},
+        include_gcp_public_datasets={'type': 'string'},
         include_org_ids={'type': 'array', 'items': {'type': 'string'}},
         include_project_ids={'type': 'array', 'items': {'type': 'string'}},
         restricted_locations={'type': 'array', 'items': {'type': 'string'}},
