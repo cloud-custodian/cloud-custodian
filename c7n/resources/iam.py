46b9d910c8dc29361e3a37bc5dfbe7c7f8b99bd5
Dave McGuire <dmcguire@blackberry.com>
2022-03-24 09:58:53 -0700

    Fix a defect where Tag filters don't work on IAM Profiles

--- c7n/resources/iam.py
+++ c7n/resources/iam.py
@@ -435,6 +435,7 @@ class InstanceProfile(QueryResourceManager):
         service = 'iam'
         arn_type = 'instance-profile'
         enum_spec = ('list_instance_profiles', 'InstanceProfiles', None)
+        detail_spec = ('get_instance_profile', 'InstanceProfileName', 'InstanceProfileName', 'InstanceProfile')
         name = id = 'InstanceProfileName'
         date = 'CreateDate'
         # Denotes this resource type exists across regions
