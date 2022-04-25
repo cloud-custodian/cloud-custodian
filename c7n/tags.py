52cf9df4a82935c66e6d8d1db48293da6a9f4e5e
thisisshi <theli@outlook.com>
2022-04-25 15:02:44 -0700

    simplify tag lookup

--- c7n/tags.py
+++ c7n/tags.py
@@ -1035,24 +1035,15 @@ class CopyRelatedResourceTag(Tag):
 
     def process(self, resources):
         related_resources = []
+        search_path = "[].[%s]" % self.data['key']
         if self.data['key'].startswith('tag:'):
             tag_key = self.data['key'].split(':', 1)[-1]
-            found = False
-            for r in resources:
-                if not r.get('Tags'):
-                    related_resources.append((None, r))
-                    continue
-                for tag in r['Tags']:
-                    if tag['Key'] == tag_key:
-                        related_resources.append((tag['Value'], r))
-                        found = True
-                        break
-                if found is False:
-                    related_resources.append((None, r))
-        else:
-            for rrid, r in zip(jmespath.search('[].[%s]' % self.data['key'], resources),
-                               resources):
-                related_resources.append((rrid[0], r))
+
+            search_path = "[].[Tags[?Key=='%s'].Value | [0]]" % tag_key
+
+        for rrid, r in zip(jmespath.search(search_path, resources),
+                           resources):
+            related_resources.append((rrid.pop(), r))
 
         related_ids = {r[0] for r in related_resources}
         missing = False
