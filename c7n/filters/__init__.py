e54349708645c1924fbdcbc17b2cf190785dcd81
AJ Kerrigan <aj@stacklet.io>
2022-06-06 23:43:46 -0400

    core - load json-diff filter by default
    The json-diff filter has some lazy logic which attaches itself to supported
    resources as they register themselves. Ensure that this filter is loaded
    early so that lazy logic fires.

--- c7n/filters/__init__.py
+++ c7n/filters/__init__.py
@@ -20,3 +20,4 @@ from .iamaccess import CrossAccountAccessFilter, PolicyChecker
 from .iamanalyzer import AccessAnalyzer
 from .metrics import MetricsFilter, ShieldMetrics
 from .vpc import DefaultVpcBase
+from .revisions import JsonDiff
