56f583fb60aeebacc8b573f55d22f1e942325b66
Kapil Thangavelu <kapilt@gmail.com>
2022-10-01 09:29:24 -0400

    github action output, policy metadata facade

--- tools/c7n_left/c7n_left/output.py
+++ tools/c7n_left/c7n_left/output.py
@@ -7,19 +7,74 @@ from rich.syntax import Syntax
 
 
 report_outputs = OutputRegistry("left")
-report_outputs.default_protocol = "rich"
+report_outputs.default_protocol = "cli"
 
 
 def get_reporter(config):
     return report_outputs.select(None, None)
 
 
-@report_outputs.register("rich")
-@report_outputs.register("default")
-class RichCli:
+class PolicyMetadata:
+    def __init__(self, policy):
+        self.policy = policy
+
+    @property
+    def resource_type(self):
+        return self.policy.resource_type
+
+    @property
+    def provider(self):
+        return self.policy.provider_name
+
+    @property
+    def name(self):
+        return self.policy
+
+    @property
+    def description(self):
+        return self.policy.data.get("description")
+
+    @property
+    def category(self):
+        return " ".join(self.policy.data.get("metadata", {}).get("category", []))
+
+    @property
+    def severity(self):
+        return self.policy.data.get("metadata", {}).get("severity", "")
+
+    @property
+    def title(self):
+        title = self.policy.data.get("metadata", {}).get("title", "")
+        if title:
+            return title
+        title = f"{self.resource_type} - policy:{self.name}"
+        if self.category:
+            title += f"category:{self.category}"
+        if self.severity:
+            title += f"severity:{self.severity}"
+        return title
+
+
+class Output:
     def __init__(self, ctx, config):
         self.ctx = ctx
         self.config = config
+
+    def on_execution_started(self, policies):
+        pass
+
+    def on_execution_ended(self):
+        pass
+
+    def on_results(self, results):
+        pass
+
+
+@report_outputs.register("cli")
+@report_outputs.register("default")
+class RichCli(Output):
+    def __init__(self, ctx, config):
+        super().__init__(ctx, config)
         self.console = Console()
         self.started = None
 
@@ -35,9 +90,6 @@ class RichCli:
     def on_results(self, results):
         for r in results:
             self.console.print(RichResult(r))
-
-    def report(self, results):
-        self.console.print(results)
 
 
 class RichResult:
@@ -60,3 +112,22 @@ class RichResult:
             lexer=resource.format,
         )
         yield ""
+
+
+class Github(Output):
+
+    # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message
+
+    "::error file={name},line={line},endLine={endLine},title={title}::{message}"
+
+    def on_results(self):
+        resource = self.policy_resource.resource
+
+        md = PolicyMetadata(self.policy)
+        filename = resource.src_dir / resource.filename
+        title = md.title
+        message = md.description
+
+        print(
+            f"::error file={filename} line={resource.line_start} lineEnd={resource.line_end} title={title}::{message}"  # noqa
+        )
