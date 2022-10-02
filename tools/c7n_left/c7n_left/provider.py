ba6ef49e054ff3df2a2c0c250bd5b45bac32ab52
Kapil Thangavelu <kapilt@gmail.com>
2022-10-02 03:25:46 -0400

    handle additional block types

--- tools/c7n_left/c7n_left/provider.py
+++ tools/c7n_left/c7n_left/provider.py
@@ -213,7 +213,10 @@ class TerraformResource(dict):
 
     def __init__(self, name, data):
         self.name = name
-        self.location = data["__tfmeta"]
+        if isinstance(data["__tfmeta"], list):
+            self.location = data["__tfmeta"][0]
+        else:
+            self.location = data["__tfmeta"]
         super().__init__(data)
 
     @property
@@ -253,18 +256,21 @@ class TerraformGraph(ResourceGraph):
     def get_resources_by_type(self, types=()):
         if isinstance(types, str):
             types = (types,)
-
         for type_name, type_items in self.resource_data.items():
             if types and type_name not in types:
                 continue
-
             if type_name == "data":
                 for data_type, data_items in type_items.items():
                     resources = []
                     for name, data in data_items.items():
                         resources.append(self.as_resource(name, data))
                     yield "%s.%s" % (type_name, data_type), resources
-
+            elif type_name == "moved":
+                yield type_name, self.as_resource(type_name, data)
+            elif type_name == "locals":
+                yield type_name, self.as_resource(type_name, data)
+            elif type_name == "terraform":
+                yield type_name, self.as_resource(type_name, data)
             else:
                 resources = []
                 for name, data in type_items.items():
@@ -272,5 +278,9 @@ class TerraformGraph(ResourceGraph):
                 yield type_name, resources
 
     def as_resource(self, name, data):
-        data["__tfmeta"]["src_dir"] = self.src_dir
+        if isinstance(data["__tfmeta"], list):
+            for m in data["__tfmeta"]:
+                m["src_dir"] = self.src_dir
+        else:
+            data["__tfmeta"]["src_dir"] = self.src_dir
         return TerraformResource(name, data)
