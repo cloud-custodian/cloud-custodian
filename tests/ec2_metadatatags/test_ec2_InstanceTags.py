237689a6986e51de3595d2bd4af2b410146ce9d8
farzanv <Farzanvahdat@gmail.com>
2022-10-02 15:02:11 -0700

    Updated

--- tests/ec2_metadatatags/test_ec2_InstanceTags.py
+++ tests/ec2_metadatatags/test_ec2_InstanceTags.py
@@ -1,5 +1,6 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
+from operator import eq
 import sys
 import os
 import logging
@@ -19,35 +20,26 @@ from tests.common import BaseTest
 from pytest_terraform import terraform
 
 
-@pytest.mark.parametrize(
-    'botocore_version',
-    ['1.26.6', '1.25.8', '0.27.27']
-)
-def test_ec2_stop_protection_lower_botocore_version_validation(test, botocore_version):
-    with mock.patch('botocore.__version__', botocore_version):
-        with test.assertRaises(PolicyValidationError) as cm:
-            policy = test.load_policy(
-                {
-                    'name': 'ec2-imds-access',
-                    'resource': 'ec2',
-                    'filters': [{'type': 'set-metadata-access'}],
-                },
-            )
-            policy.validate()
-        test.assertIn('requires botocore version 1.26.7 or above', str(cm.exception))
 
 
 @pytest.mark.parametrize(
     'botocore_version',
     ['1.26.7', '1.26.8', '1.27.0', '2.0.0']
 )
-def test_ec2_stop_protection_above_botocore_version_validation(test, botocore_version):
+def test_ec2_metadata_tags_above_botocore_version_validation(test, botocore_version):
     with mock.patch('botocore.__version__', botocore_version):
         policy = test.load_policy(
             {
                 'name': 'ec2-imds-access',
                 'resource': 'ec2',
-                'filters': [{'type': 'set-metadata-access'}],
+                'filters': [
+                {
+                    'type': 'value',
+                    'key': 'MetadataOptions.InstanceMetadataTags',
+                    'value': 'enabled',
+                    'op': 'eq'
+                }
+            ]
             },
         )
         policy.validate()
@@ -65,15 +57,11 @@ def test_ec2_metadata_tags_enabled(test, ec2_metadata_tags_enabled):
             'filters': [
                 {
                     'type': 'value',
-                    'op': 'in',
-                    'key': 'InstanceId',
-                    'value': [
-                        ec2_metadata_tags_enabled['aws_instance.metadata_tags.id'],
-                    ],
-                },
-                {'MetadataOptions.InstanceMetadataTags': 'enabled'},
-                {'type': 'set-metadata-access'},
-            ],
+                    'key': 'MetadataOptions.InstanceMetadataTags',
+                    'value': 'enabled',
+                    'op': 'eq'
+                }
+            ]
         },
         session_factory=session_factory,
         config={'region': aws_region},
@@ -82,14 +70,14 @@ def test_ec2_metadata_tags_enabled(test, ec2_metadata_tags_enabled):
     resources = p.run()
     test.assertEqual(len(resources), 1)
     test.assertEqual(
-        resources[0]['InstanceId'],
-        ec2_metadata_tags_enabled['aws_instance.metadata_tags.id'])
+        resources[0]['MetadataOptions.InstanceMetadataTags'],
+        ec2_metadata_tags_enabled['aws_instance.metadata_tags'])
 
     # set the aws_instance.metadata_tags to false to allow terraform to handle the teardown
     client = session_factory().client('ec2')
     client.modify_instance_attribute(
         InstanceId=resources[0]['InstanceId'],
-        DisableApiStop={'Value': 'enabled'}
+        DisableApiStop={'Value': False}
     )
 
 
@@ -103,16 +91,12 @@ def test_ec2_metadata_tags_disabled(test, ec2_metadata_tags_disabled):
             'name': 'ec2_metadata_tags_disabled',
             'resource': 'ec2',
             'filters': [
-                {
+                 {
                     'type': 'value',
-                    'op': 'in',
-                    'key': 'InstanceId',
-                    'value': [
-                        ec2_metadata_tags_disabled['aws_instance.metadata_tags.id'],
-                    ],
-                },
-                {'MetadataOptions.InstanceMetadataTags': 'disabled'},
-                {'not': [{'type': 'set-metadata-access'}]},
+                    'key': 'MetadataOptions.InstanceMetadataTags',
+                    'value': 'disabled',
+                    'op': 'eq'
+                }
             ],
         },
         session_factory=session_factory,
@@ -130,51 +114,12 @@ def test_ec2_metadata_tags_disabled(test, ec2_metadata_tags_disabled):
     # set the aws_instance.metadata_tags to false to allow terraform to handle the teardown
     client = session_factory().client('ec2')
     client.modify_instance_attribute(
-        InstanceId=ec2_metadata_tags_disabled['aws_instance.metadata_tags.id'],
-        DisableApiStop={'Value': 'disabled'}
+        InstanceId=resources[0]['InstanceId'],
+        DisableApiStop={'Value': False}
     )
 
 
 class TestSetMetadata(BaseTest):
-
-    def test_set_metadata_server(self):
-        output = self.capture_logging('custodian.actions')
-        session_factory = self.replay_flight_data('test_ec2_set_md_access')
-        policy = self.load_policy({
-            'name': 'ec2-imds-access',
-            'resource': 'aws.ec2',
-            'actions': [
-                {'type': 'set-metadata-access',
-                 'tokens': 'required'},
-            ]},
-            session_factory=session_factory)
-        resources = policy.run()
-        if self.recording:
-            time.sleep(2)
-        results = session_factory().client('ec2').describe_instances(
-            InstanceIds=[r['InstanceId'] for r in resources])
-        self.assertJmes('[0].MetadataOptions.HttpTokens', resources, 'optional')
-        self.assertJmes(
-            'Reservations[].Instances[].MetadataOptions',
-            results,
-            [{'HttpEndpoint': 'enabled',
-              'HttpPutResponseHopLimit': 1,
-              'HttpTokens': 'required',
-              'InstanceMetadataTags': 'disabled',
-              'State': 'pending'},
-             {'HttpEndpoint': 'enabled',
-              'HttpPutResponseHopLimit': 1,
-              'HttpTokens': 'required',
-              'InstanceMetadataTags': 'disabled',
-              'State': 'applied'}])
-        self.assertEqual(len(resources), 2)
-        self.assertEqual(
-            output.getvalue(),
-            ('set-metadata-access implicitly filtered 1 of 2 resources '
-             'key:MetadataOptions.HttpTokens on optional\n'))
-
-
-class TestSetMetadataTags(BaseTest):
 
     def test_set_metadata_server(self):
         output = self.capture_logging('custodian.actions')
@@ -192,19 +137,58 @@ class TestSetMetadataTags(BaseTest):
             time.sleep(2)
         results = session_factory().client('ec2').describe_instances(
             InstanceIds=[r['InstanceId'] for r in resources])
-        self.assertJmes('[0].MetadataOptions.InstanceMetadataTags', resources, 'disabled')
+        self.assertJmes('[0].MetadataOptions.InstanceMetadataTags', resources, 'enabled')
+        self.assertJmes(
+            'Reservations[].Instances[].MetadataOptions',
+            results,
+            [{'HttpEndpoint': 'enabled',
+              'HttpPutResponseHopLimit': 1,
+              'HttpTokens': 'required',
+              'InstanceMetadataTags': 'disabled',
+              'State': 'pending'},
+             {'HttpEndpoint': 'enabled',
+              'HttpPutResponseHopLimit': 1,
+              'HttpTokens': 'requierd',
+              'InstanceMetadataTags': 'disabled',
+              'State': 'applied'}])
+        self.assertEqual(len(resources), 2)
+        self.assertEqual(
+            output.getvalue(),
+            ('set-metadata-access implicitly filtered 1 of 2 resources '
+             'key:MetadataOptions.InstanceMetadataTags on enabled\n'))
+
+
+class TestSetMetadataTags(BaseTest):
+
+    def test_set_metadata_server(self):
+        output = self.capture_logging('custodian.actions')
+        session_factory = self.replay_flight_data('test_ec2_set_md_access')
+        policy = self.load_policy({
+            'name': 'ec2-imds-access',
+            'resource': 'aws.ec2',
+            'actions': [
+                {'type': 'set-metadata-access',
+                 'metadatatags': 'disabled'},
+            ]},
+            session_factory=session_factory)
+        resources = policy.run()
+        if self.recording:
+            time.sleep(2)
+        results = session_factory().client('ec2').describe_instances(
+            InstanceIds=[r['InstanceId'] for r in resources])
+        self.assertJmes('[0].MetadataOptions.InstanceMetadataTags', resources, 'enabled')
         self.assertJmes(
             'Reservations[].Instances[].MetadataOptions',
             results,
             [{'HttpEndpoint': 'enabled',
               'HttpPutResponseHopLimit': 1,
               'HttpTokens': 'optional',
-              'InstanceMetadataTags': 'enabled',
+              'InstanceMetadataTags': 'disabled',
               'State': 'pending'},
              {'HttpEndpoint': 'enabled',
               'HttpPutResponseHopLimit': 1,
               'HttpTokens': 'optional',
-              'InstanceMetadataTags': 'enabled',
+              'InstanceMetadataTags': 'disabled',
               'State': 'applied'}])
         self.assertEqual(len(resources), 2)
         self.assertEqual(
