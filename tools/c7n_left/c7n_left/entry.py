be70fa7a4c31a81da2be499cde999c4cc126851e
Kapil Thangavelu <kapilt@gmail.com>
2022-10-02 10:14:22 -0400

    format

--- tools/c7n_left/c7n_left/entry.py
+++ tools/c7n_left/c7n_left/entry.py
@@ -1,3 +1,3 @@
 def initialize_iaac():
     # import to get side effect registration into clouds
-    from .provider import TerraformProvider # noqa
+    from .provider import TerraformProvider  # noqa
