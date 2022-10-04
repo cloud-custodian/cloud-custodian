4db5ed3cf46ff95d681f3c2510ebcd95ebdc5616
farzanv <farzanvahdat@gmail.com>
2022-10-04 11:27:09 -0700

    Updated

--- tests/ec2_metadatatags/test_ec2_InstanceTags.py
+++ tests/ec2_metadatatags/test_ec2_InstanceTags.py
@@ -113,46 +113,6 @@ def test_ec2_metadata_tags_disabled(test, ec2_metadata_tags_disabled):
         ec2_metadata_tags_disabled['aws_instance.metadata_tags.id'],
         resource_ids)
 
-
-class TestSetMetadata(BaseTest):
-
-    def test_set_metadata_server(self):
-        output = self.capture_logging('custodian.actions')
-        session_factory = self.replay_flight_data('test_ec2_set_md_access')
-        policy = self.load_policy({
-            'name': 'ec2-imds-access',
-            'resource': 'aws.ec2',
-            'actions': [
-                {'type': 'set-metadata-access',
-                 'metadatatags': 'enabled'},
-            ]},
-            session_factory=session_factory)
-        resources = policy.run()
-        if self.recording:
-            time.sleep(2)
-        results = session_factory().client('ec2').describe_instances(
-            InstanceIds=[r['InstanceId'] for r in resources])
-        self.assertJmes('[0].MetadataOptions.InstanceMetadataTags', resources, 'enabled')
-        self.assertJmes(
-            'Reservations[].Instances[].MetadataOptions',
-            results,
-            [{'HttpEndpoint': 'enabled',
-              'HttpPutResponseHopLimit': 1,
-              'HttpTokens': 'optional',
-              'InstanceMetadataTags': 'disabled',
-              'State': 'pending'},
-             {'HttpEndpoint': 'enabled',
-              'HttpPutResponseHopLimit': 1,
-              'HttpTokens': 'requierd',
-              'InstanceMetadataTags': 'disabled',
-              'State': 'applied'}])
-        self.assertEqual(len(resources), 2)
-        self.assertEqual(
-            output.getvalue(),
-            ('set-metadata-access implicitly filtered 1 of 2 resources '
-             'key:MetadataOptions.InstanceMetadataTags on enabled\n'))
-
-
 class TestSetMetadataTags(BaseTest):
 
     def test_set_metadata_server(self):
@@ -171,7 +131,7 @@ class TestSetMetadataTags(BaseTest):
             time.sleep(2)
         results = session_factory().client('ec2').describe_instances(
             InstanceIds=[r['InstanceId'] for r in resources])
-        self.assertJmes('[0].MetadataOptions.InstanceMetadataTags', resources, 'enabled')
+        self.assertJmes('[0].MetadataOptions.InstanceMetadataTags', resources, 'disabled')
         self.assertJmes(
             'Reservations[].Instances[].MetadataOptions',
             results,
@@ -186,8 +146,8 @@ class TestSetMetadataTags(BaseTest):
               'InstanceMetadataTags': 'disabled',
               'State': 'applied'}])
         self.assertEqual(len(resources), 1)
-        self.assertEqual(resources[0]["InstanceMetadataTags"], "disabled")
-            ##output.getvalue(),
-            ##('set-metadata-access implicitly filtered 1 of 2 resources '
-            # 'key:MetadataOptions.InstanceMetadataTags on disabled\n'))
+        self.assertEqual(
+            output.getvalue(),
+            ('set-metadata-access implicitly filtered 1 of 2 resources '
+             'key:MetadataOptions.InstanceMetadataTags on disabled\n'))
    
