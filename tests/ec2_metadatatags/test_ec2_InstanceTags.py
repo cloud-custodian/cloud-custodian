49e0dc307390ac3d17f1d9dd83ff9993dbf3bf34
farzanv <farzanvahdat@gmail.com>
2022-10-05 19:46:04 -0700

    Completed Ubuntu test

--- tests/ec2_metadatatags/test_ec2_InstanceTags.py
+++ tests/ec2_metadatatags/test_ec2_InstanceTags.py
@@ -1,10 +1,21 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
-
+import logging
+import unittest
 import time
+
+import datetime
+from unittest import result
+from dateutil import tz
+import jmespath
 from mock import mock
 import pytest
 from tests.common import BaseTest
+from c7n.testing import mock_datetime_now
+from c7n.exceptions import PolicyValidationError, ClientError
+from c7n.resources import ec2
+from c7n.resources.ec2 import actions, QueryFilter
+from c7n import tags, utils
 
 
 @pytest.mark.parametrize(
@@ -16,11 +27,11 @@ def test_ec2_metadata_tags_above_botocore_version_validation(test, botocore_vers
         policy = test.load_policy(
             {
                 'name': 'ec2-imds-access',
-                'resource': 'ec2',
+                'resource': 'aws.ec2',
                 'filters': [{
                     'type': 'value',
-                    'key': 'MetadataOptions.InstanceMetadataTags',
-                    'value': 'enabled',
+                    'key': 'InstanceId',
+                    'value': 'i-045262caa95692db',
                     'op': 'eq'
                 }],
             },
@@ -36,32 +47,31 @@ class TestSetMetadataTags(BaseTest):
         policy = self.load_policy({
             'name': 'ec2-imds-access',
             'resource': 'aws.ec2',
+            'filters': [{
+                    'type': 'value',
+                    'key': 'InstanceId',
+                    'value': 'i-0d4526dcaa95692db',
+                    'op': 'eq'}],
             'actions': [
                 {'type': 'set-metadata-access',
-                 'metadatatags': 'disabled'},
+                 'metadatatags': 'enabled'},
             ]},
             session_factory=session_factory)
         resources = policy.run()
         if self.recording:
-            time.sleep(2)
+            time.sleep(10)
         results = session_factory().client('ec2').describe_instances(
             InstanceIds=[r['InstanceId'] for r in resources])
         self.assertJmes('[0].MetadataOptions.InstanceMetadataTags', resources, 'disabled')
-        self.assertJmes(
-            'Reservations[].Instances[].MetadataOptions',
-            results,
-            [{'HttpEndpoint': 'enabled',
-              'HttpPutResponseHopLimit': 1,
-              'HttpTokens': 'required',
-              'InstanceMetadataTags': 'enabled',
-              'State': 'pending'},
-             {'HttpEndpoint': 'enabled',
-              'HttpPutResponseHopLimit': 1,
+        self.assertJmes('Reservations[].Instances[].MetadataOptions', results, 
+            [{'State': 'applied', 
               'HttpTokens': 'optional',
-              'InstanceMetadataTags': 'disabled',
-              'State': 'applied'}])
-        self.assertEqual(len(resources), 2)
-        self.assertEqual(
-            output.getvalue(),
-            ('set-metadata-access implicitly filtered 1 of 2 resources '
-             'key:MetadataOptions.InstanceMetadataTags on enabled\n'))
+              'HttpEndpoint': 'enabled',
+              'HttpPutResponseHopLimit': 1,           
+              'HttpProtocolIpv6': 'disabled',
+              'InstanceMetadataTags': 'enabled'}])
+        self.assertEqual(len(resources), 1)
+
+
+        
+
