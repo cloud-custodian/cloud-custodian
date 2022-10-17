e3f5424da82bf81b3b5c3894733dac3e28f05c99
Rama Ayyagari <ayyagari74@gmail.com>
2022-10-17 11:10:48 -0700

    Added new custom SecurityContacts resource

--- tools/c7n_azure/c7n_azure/resources/resource_map.py
+++ tools/c7n_azure/c7n_azure/resources/resource_map.py
@@ -65,5 +65,6 @@ ResourceMap = {
     "azure.vm": "c7n_azure.resources.vm.VirtualMachine",
     "azure.vmss": "c7n_azure.resources.vmss.VMScaleSet",
     "azure.vnet": "c7n_azure.resources.vnet.Vnet",
-    "azure.webapp": "c7n_azure.resources.web_app.WebApp"
+    "azure.webapp": "c7n_azure.resources.web_app.WebApp",
+     "azure.security-contacts": "c7n_azure.resources.security_contacts.SecurityContacts"
 }
