77613bc8dbfa0134887f4e8f4d3465d4d5a2a302
swbsf <steven.wallimann@gmail.com>
2022-10-24 17:41:32 +0200

    fixing linting error

--- c7n/filters/offhours.py
+++ c7n/filters/offhours.py
@@ -345,7 +345,8 @@ class Time(Filter):
         self.weekends_only = self.data.get('weekends-only', False)
         self.opt_out = self.data.get('opt-out', False)
         self.tag_key = self.data.get('tag', self.DEFAULT_TAG).lower()
-        self.fallback_schedule = self.data.get('fallback-schedule', None) or self.data.get('fallback_schedule', None)
+        self.fallback_schedule = self.data.get('fallback-schedule', None) or \
+            self.data.get('fallback_schedule', None)
         self.default_schedule = self.get_default_schedule()
         self.parser = ScheduleParser(self.default_schedule)
 
