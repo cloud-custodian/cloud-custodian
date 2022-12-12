6060ff003ddfdd6300977dd4ee622e66d093cd85
Virendra Agarwal <100956047+vkagusa-mckinsey@users.noreply.github.com>
2022-12-12 16:37:06 -0500

    Update test_storage.py

--- tools/c7n_azure/tests_azure/tests_resources/test_storage.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_storage.py
@@ -650,7 +650,24 @@ class StorageTest(BaseTest):
             .client('azure.mgmt.storage.StorageManagementClient')\
             .DEFAULT_API_VERSION.replace("-", "_")
 
+    def test_storage_soft_delete(self):
+        p = self.load_policy({
+            'name': 'test-azure-storage-blob',
+            'resource': 'azure.storage',
+            'filters': [
+                {'type': 'value',
+                'key': 'name',
+                'op': 'glob',
+                'value_type': 'normalize',
+                'value': 'cctstorage*'},
+                {'type': 'soft-delete',
+                'enabled': True}],
+        }, validate=True)
 
+        resources = p.run()
+        self.assertEqual(1, len(resources))
+        
+        
 class StorageFirewallFilterTest(BaseTest):
 
     def test_query_default_allow(self):
