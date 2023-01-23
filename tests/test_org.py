a25838be59f65dfbc727046ffdb5a8425a90912f
Kapil Thangavelu <kapilt@gmail.com>
2023-01-23 13:36:25 -0500

    aws - org-account

---
+++ tests/test_org.py
@@ -0,0 +1,120 @@
+import json
+
+import boto3
+import moto
+import pytest
+
+from moto.organizations import organizations_backend
+
+template_body = json.dumps(
+    {
+        "AWSTemplateFormatVersion": "2010-09-09",
+        "Resources": {"Queue": {"Type": "AWS::SQS::Queue"}},
+    }
+)
+
+
+@pytest.fixture(scope="function")
+def org_tree(request):
+    def describe_ous():
+        for o in organizations_backend.ou:
+            print(o.describe())
+
+    with moto.mock_organizations():
+        client = boto3.client("organizations")
+        org = client.create_organization(FeatureSet="ALL")["Organization"]
+        root = client.list_roots()["Roots"][0]
+
+        dept_a = client.create_organizational_unit(ParentId=org["Id"], Name="DeptA")[
+            "OrganizationalUnit"
+        ]
+        dept_b = client.create_organizational_unit(ParentId=org["Id"], Name="DeptB")[
+            "OrganizationalUnit"
+        ]
+        group_c = client.create_organizational_unit(
+            ParentId=dept_a["Id"], Name="GroupC"
+        )["OrganizationalUnit"]
+
+        describe_ous()
+        account_a = client.create_account(
+            Email="a@example.com",
+            AccountName="a",
+            Tags=[{"Key": "Owner", "Value": "alice"}],
+        )["CreateAccountStatus"]
+
+        describe_ous()
+        client.move_account(
+            AccountId=account_a["AccountId"],
+            SourceParentId=root["Id"],
+            DestinationParentId=dept_a["Id"],
+        )
+
+        account_b = client.create_account(
+            Email="b@example.com",
+            AccountName="b",
+            Tags=[{"Key": "Owner", "Value": "bob"}],
+        )["CreateAccountStatus"]
+
+        client.move_account(
+            AccountId=account_b["AccountId"],
+            SourceParentId=root["Id"],
+            DestinationParentId=dept_b["Id"],
+        )
+
+        account_c = client.create_account(
+            Email="b@example.com",
+            AccountName="c",
+            Tags=[{"Key": "Owner", "Value": "eve"}],
+        )["CreateAccountStatus"]
+
+        client.move_account(
+            AccountId=account_c["AccountId"],
+            SourceParentId=root["Id"],
+            DestinationParentId=group_c["Id"],
+        )
+
+        yield dict(
+            org=org,
+            dept_a=dept_a,
+            dept_b=dept_b,
+            group_c=group_c,
+            account_a=account_a,
+            account_b=account_b,
+            account_c=account_c,
+            root=root,
+        )
+
+
+def test_org_account_ou_filter(test, org_tree):
+    p = test.load_policy(
+        {
+            "name": "accounts",
+            "resource": "aws.org-account",
+            "filters": [{"type": "ou", "units": [org_tree["dept_a"]["Id"]]}],
+        }
+    )
+    resources = p.run()
+    assert {r["Id"] for r in resources} == {
+        org_tree["account_a"]["AccountId"],
+        org_tree["account_c"]["AccountId"],
+    }
+
+
+def test_org_account_moto(test, org_tree):
+    p = test.load_policy(
+        {
+            "name": "accounts",
+            "resource": "aws.org-account",
+        },
+    )
+    resources = p.run()
+    assert len(resources) == 4
+    p = test.load_policy(
+        {
+            "name": "accounts",
+            "resource": "aws.org-account",
+            "filters": [{"tag:Owner": "eve"}],
+        },
+    )
+    resources = p.run()
+    assert len(resources) == 1
