178961aa4b2e73d873ea2047418a43fb06b5c7eb
Kapil Thangavelu <kapilt@gmail.com>
2023-02-11 05:56:04 -0500

    flake8 clean

--- c7n/resources/org.py
+++ c7n/resources/org.py
@@ -9,7 +9,7 @@ from c7n.credentials import assumed_session
 from c7n.executor import MainThreadExecutor
 from c7n.filters import Filter
 from c7n.query import QueryResourceManager, TypeInfo
-from c7n.resources.aws import AWS, ResourceMap
+from c7n.resources.aws import AWS
 from c7n.utils import local_session, type_schema
 
 
