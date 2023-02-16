28832448a0122923a6b201da92d7190fda7c712a
Kapil Thangavelu <kapilt@gmail.com>
2023-02-16 13:19:33 -0500

    lint

--- tests/test_output.py
+++ tests/test_output.py
@@ -7,12 +7,10 @@ import mock
 import shutil
 import os
 
-from contextlib import nullcontext as no_exception
 from dateutil.parser import parse as date_parse
 
 from c7n.ctx import ExecutionContext
 from c7n.config import Config
-from c7n.exceptions import InvalidOutputConfig
 from c7n.output import DirectoryOutput, BlobOutput, LogFile, metrics_outputs
 from c7n.resources.aws import S3Output, MetricsOutput, get_bucket_region_clientless
 from c7n.testing import mock_datetime_now, TestUtils
