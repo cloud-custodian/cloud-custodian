f36b5320c733e070e79c63cb6357a1d7d58d466b
Jorge Bianquetti <jbianquetti@vmware.com>
2023-04-13 11:36:53 +0200

    Adding resource azure.open-shift for Azure RedHat Openshift clusters
    Signed-off-by: Jorge Bianquetti <jbianquetti@vmware.com>

--- tools/c7n_azure/c7n_azure/resources/open_shift.py
+++ tools/c7n_azure/c7n_azure/resources/open_shift.py
@@ -30,13 +30,13 @@ class OpenShiftService(ArmResourceManager):
     """
 
     class resource_type(ArmResourceManager.resource_type):
-        doc_groups = ['Containers']
+        doc_groups = ['Compute', 'Containers']
         service = 'azure.mgmt.redhatopenshift'
         client = 'AzureRedHatOpenShiftClient'
         enum_spec = ('open_shift_clusters', 'list', None)
         default_report_fields = (
             'name',
             'location',
-            'properties.version'
+            'resourceGroup'
         )
         resource_type = 'Microsoft.RedHatOpenShift/openShiftClusters'
