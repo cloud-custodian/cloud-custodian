f031cf35108f7f7ce8e9465febb0f4d01c85c79f
femio27 <102007964+femio27@users.noreply.github.com>
2023-05-23 14:56:44 -0500

    Updated policy for recovery services

--- tools/c7n_azure/c7n_azure/resources/recovery_services.py
+++ tools/c7n_azure/c7n_azure/resources/recovery_services.py
@@ -7,6 +7,23 @@ from c7n_azure.resources.arm import ArmResourceManager
 
 @resources.register('recovery-services')
 class RecoveryServices(ArmResourceManager):
+    """Azure Recovery Services Vault
+
+    :example:
+
+    Return all Recovery Services Vault on the Standcard SKU
+
+    .. code-block:: yaml
+
+        policies:
+          - name: recovery-services-vault
+            resource: azure.recovery-services
+            filters: value
+            key: sku.name
+            op: equal
+            value: Standard
+
+    """
 
     class resource_type(ArmResourceManager.resource_type):
         doc_groups = ['Backup and Recovery']
