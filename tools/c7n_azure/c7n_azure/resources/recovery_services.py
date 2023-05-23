7f69c063f18061c7bf841ddcfa2821675b5243e2
femio27 <102007964+femio27@users.noreply.github.com>
2023-05-23 08:58:47 -0500

    Added resource for azure recovery services vault

---
+++ tools/c7n_azure/c7n_azure/resources/recovery_services.py
@@ -0,0 +1,24 @@
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+
+from c7n_azure.provider import resources
+from c7n_azure.resources.arm import ArmResourceManager
+
+
+@resources.register('recovery-services')
+class RecoveryServices(ArmResourceManager):
+
+    class resource_type(ArmResourceManager.resource_type):
+        doc_groups = ['Recovery Services']
+
+        service = 'azure.mgmt.recoveryservices'
+        client = 'RecoveryServicesClient'
+        enum_spec = ('vaults', 'list_by_subscription_id', None)
+        default_report_fields = (
+            'name',
+            'location',
+            'resourceGroup',
+            'sku.name'
+        )
+
+        resource_type = 'Microsoft.RecoveryServices/vaults'
