b58da900b4c6c6189416fe0d6204a43bf9dd95bc
Kapil Thangavelu <kapilt@gmail.com>
2023-05-28 06:29:06 -0400

    fix typo

--- tools/c7n_gcp/c7n_gcp/client.py
+++ tools/c7n_gcp/c7n_gcp/client.py
@@ -182,7 +182,7 @@ class Session:
             self._use_cached_http = True
             default_credentials, _ = google.auth.default(quota_project_id=project_id or
             get_default_project())
-        impersonate_credentials = None
+        impersonated_credentials = None
         if impersonate_service or GOOGLE_IMPERSONATE_SERVICE_ACCOUNT:
             impersonate_target = impersonate_service or GOOGLE_IMPERSONATE_SERVICE_ACCOUNT
             log.info('using impersonated service account %s', impersonate_target)
