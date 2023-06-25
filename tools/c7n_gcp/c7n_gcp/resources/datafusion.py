b4eb0874dc8ce56fc3381d42c2a779d3f751d299
Yauhen Shaliou <yauhen_shaliou@epam.com>
2023-06-25 16:46:50 +0300

    gcp - added datafusion

---
+++ tools/c7n_gcp/c7n_gcp/resources/datafusion.py
@@ -0,0 +1,19 @@
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+from c7n_gcp.provider import resources
+from c7n_gcp.query import (QueryResourceManager, TypeInfo)
+
+
+@resources.register('datafusion-instance')
+class DatafusionInstance(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'datafusion'
+        version = 'v1'
+        component = 'projects.locations.instances'
+        enum_spec = ('list', 'instances[]', None)
+        scope = 'project'
+        scope_key = 'parent'
+        scope_template = "projects/{}/locations/-"
+        name = id = "name"
+        default_report_fields = ['name', 'updateTime']
