dd47af73dde4c5a39f6159e96ecdc5f8470fcf07
Naohito Takeuchi <104223034+naohito-intuit@users.noreply.github.com>
2023-07-13 11:31:44 -0700

    remove config_type

--- c7n/resources/ec2.py
+++ c7n/resources/ec2.py
@@ -2218,7 +2218,7 @@ class LaunchTemplate(query.QueryResourceManager):
         filter_name = 'LaunchTemplateIds'
         filter_type = 'list'
         arn_type = "launch-template"
-        cfn_type = config_type = "AWS::EC2::LaunchTemplate"
+        cfn_type = "AWS::EC2::LaunchTemplate"
 
     def augment(self, resources):
         client = utils.local_session(
