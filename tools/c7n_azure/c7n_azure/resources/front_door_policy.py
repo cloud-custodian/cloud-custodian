6dfc007fcf7038c667ecdbe640486216318925d5
Carol Brothers <carol_brothers@mckinsey.com>
2023-08-08 16:03:56 -0700

    Create new resource front-door-policy to get frontdoorWebApplicationFirewallPolicies managed rule disabled

---
+++ tools/c7n_azure/c7n_azure/resources/front_door_policy.py
@@ -0,0 +1,37 @@
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+
+from c7n_azure.resources.arm import ArmResourceManager
+from c7n_azure.provider import resources
+from c7n.filters import Filter
+from c7n.utils import type_schema
+
+
+@resources.register('front-door-policy')
+class FrontDoorPolicy(ArmResourceManager):
+    """Azure Front Door Resource
+
+    :example:
+
+    This policy will find all Front Doors
+
+    .. code-block:: yaml
+
+        policies:
+          - name: all-front-doors-policies
+            resource: azure.front-door-policy
+    """
+
+    class resource_type(ArmResourceManager.resource_type):
+        doc_groups = ['Network']
+
+        service = 'azure.mgmt.frontdoor'
+        client = 'FrontDoorManagementClient'
+        enum_spec = ('policies', 'list_by_subscription', None)
+        default_report_fields = (
+            'name',
+            'location',
+            'resourceGroup'
+        )
+        resource_type = 'Microsoft.Network/frontDoorWebApplicationFirewallPolicies'
+
