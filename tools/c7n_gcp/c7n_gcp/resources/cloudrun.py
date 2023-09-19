f11ff7e29dc928460cb7224c8aafec65f1222435
Kapil Thangavelu <kapilt@gmail.com>
2023-09-19 08:06:59 -0400

    gcp - cloudrun v2 service

--- tools/c7n_gcp/c7n_gcp/resources/cloudrun.py
+++ tools/c7n_gcp/c7n_gcp/resources/cloudrun.py
@@ -1,7 +1,45 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
+from c7n.actions import SetLabelsAction
 from c7n_gcp.provider import resources
-from c7n_gcp.query import QueryResourceManager, TypeInfo
+from c7n_gcp.query import ChildResourceManager, QueryResourceManager, TypeInfo
+
+@resources.register('cloud-run-service-v2')
+class CloudRunServiceV2(ChildResourceManager):
+
+    class resource_type(TypeInfo):
+        service = "run"
+        version = "v2"
+        component = "projects.locations.services"
+        name = id = "name"
+        parent_spec = {
+            'resource': 'region',
+            'child_enum_params': {
+                ('name', 'region')
+            },
+            'use_child_query': True
+        }
+        default_report_fields = ('name', 'description', 'updateTime', 'lastModified', 'observedGeneration')
+        urn_component = "services"
+        urn_id_segments = (-1,)
+        asset_type = "run.googleapis.com/Service"
+
+        @classmethod
+        def _get_location(cls, resource):
+            resource['name'].split('/')[3]
+
+
+@CloudRunServiceV2.register('set-labels')
+class SetServiceLabels(SetLabelsAction):
+
+    method_spec = {'': ''}
+    method_perm = 'patch'
+
+    def get_resource_params(self, model, resource):
+        pass
+
+    def process(self, resources):
+        pass
 
 
 @resources.register("cloud-run-service")
