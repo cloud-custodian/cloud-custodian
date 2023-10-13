94baf0c52cd204f95a1b83afc41b16464f248387
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2023-10-13 16:37:02 +0300

    Add docstring to signalr

--- tools/c7n_azure/c7n_azure/resources/signalr.py
+++ tools/c7n_azure/c7n_azure/resources/signalr.py
@@ -4,6 +4,25 @@ from c7n_azure.resources.arm import ArmResourceManager
 
 @resources.register('signalr')
 class Signalr(ArmResourceManager):
+    """Signalr Resource
+
+    :example:
+
+    Finds SignalR service without Private Endpoint connection configured
+
+    .. code-block:: yaml
+
+        policies:
+          - name: asb_signalr_private_link
+            resource: azure.signalr
+            filters:
+              - not:
+                  - type: value
+                    key: properties.privateEndpointConnections[].properties.privateLinkServiceConnectionState.status
+                    value: Approved
+                    op: contains
+
+    """
     class resource_type(ArmResourceManager.resource_type):
         doc_groups = ['Networking']
         service = 'azure.mgmt.signalr'
