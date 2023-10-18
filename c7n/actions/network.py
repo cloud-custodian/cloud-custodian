3dfd95be26c3b4e1daf4be3d39b1c8f716df3288
Pratyush Mishra <pratyush.mishra@capitalone.com>
2023-10-18 13:14:22 -0400

    Look for the right VPC ID for the resource type

--- c7n/actions/network.py
+++ c7n/actions/network.py
@@ -92,6 +92,12 @@ class ModifyVpcSecurityGroupsAction(Action):
     sg_expr = None
     vpc_expr = None
 
+    def get_vpc_related_id(self):
+        vpc_filter = self.manager.filter_registry.get('vpc')
+        if not vpc_filter or not vpc_filter.RelatedIdsExpression:
+            return
+        return utils.jmespath_compile(vpc_filter.RelatedIdsExpression)
+
     def validate(self):
         sg_filter = self.manager.filter_registry.get('security-group')
         if not sg_filter or not sg_filter.RelatedIdsExpression:
@@ -99,12 +105,11 @@ class ModifyVpcSecurityGroupsAction(Action):
                 "policy:{policy} resource:{resource_type} does "
                 "not support {action_type} action")))
         if self.get_action_group_names():
-            vpc_filter = self.manager.filter_registry.get('vpc')
-            if not vpc_filter or not vpc_filter.RelatedIdsExpression:
+            if not self.get_vpc_related_id():
                 raise PolicyValidationError(self._format_error((
                     "policy:{policy} resource:{resource_type} does not support "
                     "security-group names only ids in action:{action_type}")))
-            self.vpc_expr = utils.jmespath_compile(vpc_filter.RelatedIdsExpression)
+            self.vpc_expr = self.get_vpc_related_id()
         if self.sg_expr is None:
             self.sg_expr = utils.jmespath_compile(
                 self.manager.filter_registry.get('security-group').RelatedIdsExpression)
@@ -264,7 +269,7 @@ class ModifyVpcSecurityGroupsAction(Action):
                 r, self._get_array('isolation-group'), resolved_groups)
 
             for sg in tag_filtered_groups:
-                if sg['VpcId'] == r['VpcId']:
+                if sg['VpcId'] == self.get_vpc_related_id().search(r):
                     add_groups.append(sg['GroupId'])
 
             for g in remove_groups:
