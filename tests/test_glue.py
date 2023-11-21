f9123f8bb86a1ce5008f272de350c52294242cb4
timmygrable <59906410+timmygrable@users.noreply.github.com>
2023-11-21 17:05:15 -0500

    add test to cover codecov

--- tests/test_glue.py
+++ tests/test_glue.py
@@ -102,6 +102,26 @@ class TestGlueConnections(BaseTest):
         tags = client.get_tags(ResourceArn=arn)
         self.assertEqual(tags.get('Tags'), {})
 
+    def test_glue_query_resources_no_subnet(self):
+        session_factory = self.replay_flight_data("test_glue_query_resources_no_subnet")
+        p = self.load_policy(
+            {
+                "name": "list-glue-connections",
+                "resource": "glue-connection",
+                "filters": [
+                    {
+                        "type": "subnet",
+                        "key": "tag:c7n-internal",
+                        "op": "ne",
+                        "value": "True",
+                    }
+                ],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 0)
+
 
 class TestGlueDevEndpoints(BaseTest):
 
