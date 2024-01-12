a955dfa9c80482d137e528e1d84b5616b897b176
Kapil Thangavelu <kapilt@gmail.com>
2024-01-12 08:51:50 -0500

    Update synapse.py
    update doc group

--- tools/c7n_azure/c7n_azure/resources/synapse.py
+++ tools/c7n_azure/c7n_azure/resources/synapse.py
@@ -5,7 +5,7 @@ from c7n_azure.resources.arm import ArmResourceManager
 @resources.register('synapse')
 class Synapse(ArmResourceManager):
     class resource_type(ArmResourceManager.resource_type):
-        doc_groups = ['Networking']
+        doc_groups = ['Analytics']
         service = 'azure.mgmt.synapse'
         client = 'SynapseManagementClient'
         enum_spec = ('workspaces', 'list', None)
