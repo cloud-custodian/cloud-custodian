6621710a7be2169c27aba256d50e8070da9528cc
shakiyal <shakkalakka12@gmail.com>
2024-04-02 18:06:47 +0100

    linter fixes

--- c7n/resources/chimesdkvoice.py
+++ c7n/resources/chimesdkvoice.py
@@ -1,15 +1,11 @@
 from c7n.manager import resources
 from c7n.query import (
-    ChildResourceManager,
     QueryResourceManager,
     TypeInfo,
     DescribeSource,
-    ChildDescribeSource,
     ConfigSource,
 )
-from c7n.resources.aws import Arn
 from c7n.tags import universal_augment
-from c7n.utils import local_session
 
 
 class DescribeVoiceConnector(DescribeSource):
@@ -19,11 +15,11 @@ class DescribeVoiceConnector(DescribeSource):
         return tagged_resources
 
 
-
 @resources.register('chimesdkvoice-voiceconnector')
 class VoiceConnector(QueryResourceManager):
     source_mapping = {'describe': DescribeVoiceConnector,
                       'config': ConfigSource}
+
     # interior class that defines the aws metadata for resource
     class resource_type(TypeInfo):
         service = 'chime-sdk-voice'
--- tests/test_chimesdkvoice.py
+++ tests/test_chimesdkvoice.py
@@ -44,7 +44,8 @@ class TestChimeSDKVoice(BaseTest):
                  'OutboundHostName': 'gp9yxbfhynquklulp0dyi4.voiceconnector.chime.aws',
                  'RequireEncryption': False,
                  'UpdatedTimestamp': datetime(2024, 4, 2, 13, 35, 10, 202000, tzinfo=timezone.utc),
-                 'VoiceConnectorArn': 'arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4',
+                 'VoiceConnectorArn':
+                     'arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4',
                  'VoiceConnectorId': 'gp9yxbfhynquklulp0dyi4'}
 
             ],
@@ -71,12 +72,13 @@ class TestChimeSDKVoice(BaseTest):
             [
                 {'operation': 'ListVoiceConnectors', 'params': {}, 'service': 'chime-sdk-voice'},
                 {'operation': 'GetResources',
-                 'params': {'ResourceARNList': ['arn:aws:chime:us-east-1:644160558196:vc/ddvk97wedue2gzbz8eeeri',
-                                                'arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4']},
+                 'params':
+                     {'ResourceARNList':
+                          ['arn:aws:chime:us-east-1:644160558196:vc/ddvk97wedue2gzbz8eeeri',
+                           'arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4']},
                  'service': 'resourcegroupstaggingapi'}],
             captor.calls,
         )
-
 
     def test_chimesdkvoice_event(self):
         session_factory = self.replay_flight_data('test_chimesdkvoice_voiceconnector')
@@ -126,12 +128,15 @@ class TestChimeSDKVoice(BaseTest):
                     'Tags': [{'Key': 'my-key', 'Value': 'my-value'}],
                     'c7n:MatchedFilters': ['RequireEncryption'],
                     'AwsRegion': 'us-east-1',
-                    'CreatedTimestamp': datetime(2024, 4, 2, 13, 4, 22, 308000, tzinfo=timezone.utc),
+                    'CreatedTimestamp':
+                        datetime(2024, 4, 2, 13, 4, 22, 308000, tzinfo=timezone.utc),
                     'Name': 'vc-encryption-off',
                     'OutboundHostName': 'gp9yxbfhynquklulp0dyi4.voiceconnector.chime.aws',
                     'RequireEncryption': False,
-                    'UpdatedTimestamp': datetime(2024, 4, 2, 13, 35, 10, 202000, tzinfo=timezone.utc),
-                    'VoiceConnectorArn': 'arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4',
+                    'UpdatedTimestamp':
+                        datetime(2024, 4, 2, 13, 35, 10, 202000, tzinfo=timezone.utc),
+                    'VoiceConnectorArn':
+                        'arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4',
                     'VoiceConnectorId': 'gp9yxbfhynquklulp0dyi4'
                 }
             ],
@@ -142,7 +147,8 @@ class TestChimeSDKVoice(BaseTest):
         # deriving the ARN.
         # See the documentation on the "arn" field in appmesh.py.
         arns = p.resource_manager.get_arns(resources)
-        self.assertEqual(['arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4'], arns)
+        self.assertEqual(['arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4'],
+                         arns)
 
         # The "placebo" testing library doesn't allow us to make assertions
         # linking specific api's calls to the specific mock response file
@@ -151,15 +157,16 @@ class TestChimeSDKVoice(BaseTest):
         # of calls that must be made.
         self.assertEqual(
             [
-                {'operation': 'ListVoiceConnectors', 'params': {}, 'service': 'chime-sdk-voice'},
+                {'operation':
+                     'ListVoiceConnectors', 'params': {}, 'service': 'chime-sdk-voice'},
                 {'operation': 'GetResources',
-                 'params': {'ResourceARNList': ['arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4']},
+                 'params':
+                     {'ResourceARNList':
+                          ['arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4']},
                  'service': 'resourcegroupstaggingapi'},
             ],
             captor.calls,
         )
-
-
 
     def test_reporting(self):
         f = Formatter(resource_type=VoiceConnector.resource_type)
@@ -175,7 +182,7 @@ class TestChimeSDKVoice(BaseTest):
                  'OutboundHostName': 'gp9yxbfhynquklulp0dyi4.voiceconnector.chime.aws',
                  'RequireEncryption': False,
                  'UpdatedTimestamp': datetime(2024, 4, 2, 13, 35, 10, 202000, tzinfo=timezone.utc),
-                 'VoiceConnectorArn': 'arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4',
+                 'VoiceConnectorArn': 'arn:aws:chime:us-east-1:644160558196:vc/gp9yxbfhynquklulp0dyi4',  # noqa
                  'VoiceConnectorId': 'gp9yxbfhynquklulp0dyi4'}
 
             ]
