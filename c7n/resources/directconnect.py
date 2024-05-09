9aa76cd939f72a3dde4035c71834740d22998e83
armichaud <email@alexandermichaud.tech>
2024-05-09 08:36:20 -0400

    register gateway association

--- c7n/resources/directconnect.py
+++ c7n/resources/directconnect.py
@@ -30,9 +30,25 @@ class DirectConnectVirtualInterface(QueryResourceManager):
         id = 'virtualInterfaceId'
         name = 'virtualInterfaceName'
         filter_name = 'virtualInterfaceId'
-        filter_type = 'scalar'
+        filter_type = 'scalar' # TODO list?
         arn_type = "dxvif"
         universal_taggable = object()
         permissions_augment = ("directconnect:DescribeTags",)
 
     augment = universal_augment
+
+    @resources.register('directconnect-gateway-association')
+    class DirectConnectGatewayAssociation(QueryResourceManager):
+
+        class resource_type(TypeInfo):
+            service = 'directconnect'
+            enum_spec = ('describe_direct_connect_gateway_associations', 'directConnectGatewayAssociations', None)
+            id = 'associationId'
+            name = 'associationId'
+            filter_name = 'associationId'
+            filter_type = 'scalar'
+            arn_type = 'dx-gateway'
+            universal_taggable = object()
+            permissions_augment = ("directconnect:DescribeTags",)
+
+        augment = universal_augment
