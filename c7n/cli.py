2f2ae7e7439b2df47287334c39c003747f393638
Pratyush Mishra <pratyush.mishra@capitalone.com>
2024-05-17 06:39:50 -0500

    Parameter help updated to reflect AWS provider

--- c7n/cli.py
+++ c7n/cli.py
@@ -102,7 +102,7 @@ def _default_options(p, exclude=[]):
     if 'session-policy' not in exclude:
         p.add_argument("--session-policy", required=False, default=None,
                        action=LoadSessionPolicyJson,
-                       help="[OPTIONAL] Policy Document to be used as a session policy")
+                       help="[OPTIONAL] AWS IAM Policy Document to be used as a session policy")
 
 
 def _report_options(p):
