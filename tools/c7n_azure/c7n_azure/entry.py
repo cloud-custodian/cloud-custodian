3be37e7de8d6bd2b72216d7a0171d38c426c8a08
Kapil Thangavelu <kapilt@gmail.com>
2024-06-04 05:47:00 -0400

    work around azure workings on 3.12

--- tools/c7n_azure/c7n_azure/entry.py
+++ tools/c7n_azure/c7n_azure/entry.py
@@ -1,6 +1,12 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
 
+# two azure packages seem to have this issue on invalid escape
+# sequence on their generated code w/ python 3.12
+# (azure.mgmt.resource, azure.mgmt.resourcgraph)
+import warnings
+warnings.filterwarnings("ignore", "SyntaxWarning")
+
 # register provider
 from c7n_azure.provider import Azure  # NOQA
 
