1322e8e55d6e160d0f5ed7c6ff4fe1f8ffbd06ce
AJ Kerrigan <aj@stacklet.io>
2024-06-24 10:46:05 -0400

    aws - set-flow-log action - keep attrs keys, use model ids during delete
    - Allow the `attrs` keys to live through legacy schema conversion, and
      take precedence over legacy schema keys
    
    - When building a list of resource IDs for flow log deletion, use model
      information from the resource manager
    
    - Gracefully account for already-deleted flow logs

--- tests/test_vpc.py
+++ tests/test_vpc.py
@@ -3907,6 +3907,52 @@ class FlowLogsTest(BaseTest):
         ]
         self.assertEqual(logs[0]["ResourceId"], resources[0]["VpcId"])
 
+    def test_vpc_set_flow_logs_legacy_schema_handling(self):
+        session_factory = self.replay_flight_data("test_vpc_set_flow_logs_legacy_schema_handling")
+        p = self.load_policy(
+            {
+                "name": "c7n-vpc-flow-logs-legacy-schema",
+                "resource": "aws.vpc",
+                "filters": [
+                    {"tag:Name": "FlowLogTest"}, {"type": "flow-logs", "enabled": False}
+                ],
+                "actions": [
+                    {
+                        "type": "set-flow-log",
+                        "LogDestinationType": "invalid-value",
+                        "LogDestination": "arn:aws:s3:::nonsense",
+                        "TrafficType": "ALL",
+                        "attrs": {
+                            "LogDestinationType": "s3",
+                            "LogDestination": "arn:aws:s3:::c7n-test/test.log.gz",
+                            "TagSpecifications": [
+                                {
+                                    "ResourceType": "vpc-flow-log",
+                                    "Tags": [
+                                        {
+                                            "Key": "Name",
+                                            "Value": "FlowLogTest"
+                                        },
+                                    ]
+                                }
+                            ]
+                        }
+                    }
+                ],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0]["VpcId"], "vpc-05ee7d52fda39ac81")
+        client = session_factory(region="us-east-1").client("ec2")
+        logs = client.describe_flow_logs(
+            Filters=[{"Name": "resource-id", "Values": [resources[0]["VpcId"]]}]
+        )[
+            "FlowLogs"
+        ]
+        self.assertEqual(logs[0]["ResourceId"], resources[0]["VpcId"])
+
     def test_vpc_delete_flow_logs(self):
         session_factory = self.replay_flight_data("test_vpc_delete_flow_logs")
         p = self.load_policy(
