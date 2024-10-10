b4cf2d7db2354a37ab643c3a65ca2ddbdf61cdff
nabeelmohamed <mohamednabeel321@gmail.com>
2024-10-10 10:28:57 +0000

    updated emr_serverless_jobs file

--- tools/c7n_awscc/c7n_awscc/resources/emr_serverless_jobs.py
+++ tools/c7n_awscc/c7n_awscc/resources/emr_serverless_jobs.py
@@ -1,33 +1,5 @@
-from c7n.provider import resources
+from c7n.provider import resources, local_session
 from c7n.query import QueryResourceManager
-from c7n.utils import type_schema
-
-@resources.register('emr-serverless-jobs')
-class EMRServerlessJobs(QueryResourceManager):
-
-    class resource_type:
-        service = 'emr'
-        enum_spec = ('list_jobs', 'Jobs', None)
-        detail_spec = ('describe_job', 'JobId', 'JobId', 'Job')
-        id = 'Id'
-        name = 'Name'
-        date = 'CreatedOn'
-        dimension = None
-from c7n.provider import resources
-from c7n.query import QueryResourceManager
-from c7n.utils import type_schema
-
-@resources.register('emr-serverless-jobs')
-class EMRServerlessJobs(QueryResourceManager):
-
-    class resource_type:
-        service = 'emr'
-        enum_spec = ('list_jobs', 'Jobs', None)
-        detail_spec = ('describe_job', 'JobId', 'JobId', 'Job')
-        id = 'Id'
-        name = 'Name'
-        date = 'CreatedOn'
-        dimension = None
 
 METRICS = ['JobRunTime', 'WorkersUsed']
 
@@ -42,6 +14,7 @@ class EMRServerlessJobs(QueryResourceManager):
         name = 'Name'
         date = 'CreatedOn'
         dimension = None
+
 
     def get_metrics(self):
         client = local_session(self.session_factory).client('emr')
