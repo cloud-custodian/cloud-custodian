a705d062530ecd821d19e350a25a6ece50a3611a
Zachary Karpinski <zkarpinski@protonmail.com>
2024-12-09 14:05:44 -0500

    added 3 new resources

--- c7n/resources/athena.py
+++ c7n/resources/athena.py
@@ -16,3 +16,45 @@ class AthenaNamedQuery(query.QueryResourceManager):
         name = "Name"
         cfn_type = "AWS::Athena::NamedQuery"
         permissions_augment = ("athena:ListTagsForResource",)
+
+
+@resources.register("athena-workgroup")
+class AthenaWorkGroup(query.QueryResourceManager):
+
+    class resource_type(query.TypeInfo):
+        service = "athena"
+        enum_spec = ('list_work_groups', 'WorkGroups', None)
+        detail_spec = ('get_work_group', 'Name', 'Name', 'WorkGroup')
+        arn = "Arn"
+        id = "Name"
+        name = "Name"
+        cfn_type = "AWS::Athena::WorkGroup"
+        permissions_augment = ("athena:ListTagsForResource",)
+
+
+@resources.register("athena-data-catalog")
+class AthenaDataCatalog(query.QueryResourceManager):
+
+    class resource_type(query.TypeInfo):
+        service = "athena"
+        enum_spec = ('list_data_catalogs', 'DataCatalogsSummary', None)
+        detail_spec = ('get_data_catalog', 'Name', 'Name', 'DataCatalog')
+        arn = "Arn"
+        id = "Name"
+        name = "Name"
+        cfn_type = "AWS::Athena::DataCatalog"
+        permissions_augment = ("athena:ListTagsForResource",)
+
+
+@resource.register('athena-capacity-reservation')
+class AthenaCapacityReservation(query.QueryResourceManager):
+
+    class resource_type(query.TypeInfo):
+        service = 'athena'
+        enum_spec = ('list_capacity_reservations', 'CapacityReservations', None)
+        detail_spec = ('get_capacity_reservation', 'CapacityReservationId', 'CapacityReservationId', 'CapacityReservation')
+        arn = 'CapacityReservationArn'
+        id = 'CapacityReservationId'
+        name = 'CapacityReservationId'
+        cfn_type = 'AWS::Athena::CapacityReservation'
+        permissions_augment = ('athena:ListTagsForResource',)
