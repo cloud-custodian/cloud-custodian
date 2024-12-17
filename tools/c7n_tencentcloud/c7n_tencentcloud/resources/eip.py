f10854df7fa55ad19a071080a3106c8f6c0c9c09
MaDongdong <donaldma@tencent.com>
2024-12-17 15:14:38 +0800

    c7n_tencentcloud - resources - EIP & DnsRecords

---
+++ tools/c7n_tencentcloud/c7n_tencentcloud/resources/eip.py
@@ -0,0 +1,39 @@
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+from c7n.utils import type_schema
+
+from c7n.filters import Filter, ValueFilter
+
+from c7n_tencentcloud.provider import resources
+from c7n_tencentcloud.query import ResourceTypeInfo, QueryResourceManager
+from c7n_tencentcloud.utils import PageMethod
+
+
+@resources.register("eip")
+class EIP(QueryResourceManager):
+    """eip
+    Elastic IP address (EIP)
+    https://www.tencentcloud.com/document/product/215/16702
+
+    :example:
+
+    .. code-block::
+
+    policies:
+      - name: tencentcloud-eip
+        resource: tencentcloud.eip
+        filters:
+          - AddressIp: "43.159.34.197"
+
+    """
+
+    class resource_type(ResourceTypeInfo):
+        """resource_type"""
+        id = "AddressId"
+        endpoint = "vpc.tencentcloudapi.com"
+        service = "vpc"
+        version = "2017-03-12"
+        enum_spec = ("DescribeAddresses", "Response.AddressSet[]", {})
+        paging_def = {"method": PageMethod.Offset, "limit": {"key": "Limit", "value": 20}}
+        resource_prefix = "eip"
+        taggable = True
