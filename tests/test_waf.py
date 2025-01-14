086c776c72feb9170acbc2664c2c596588593025
Aman <amanvir7@gmail.com>
2025-01-14 14:01:11 -0500

    Added more tests

--- tests/test_waf.py
+++ tests/test_waf.py
@@ -124,3 +124,127 @@ class WAFTest(BaseTest):
         self.assertEqual(logging_config["LoggingConfiguration"]["ResourceArn"], resources[0]["ARN"])
         self.assertEqual(logging_config["LoggingConfiguration"]["LogDestinationConfigs"][0],
                          "arn:aws:s3:::aws-waf-logs-test-custodian-creation")
+
+    def test_wafv2_enable_logging_with_redacted_fields(self):
+        session_factory = self.replay_flight_data("test_wafv2_enable_logging_with_redacted_fields")
+        policy = {
+            "name": "wafv2-enable-logging-redacted-fields",
+            "resource": "aws.wafv2",
+            "filters": [
+                {
+                    "type": "value",
+                    "key": "Name",
+                    "value": "test-custodian-waf",
+                    "op": "eq"
+                }
+            ],
+            "actions": [
+                {
+                    "type": "enable-logging",
+                    "log_destination_arn": "arn:aws:s3:::aws-waf-logs-test-custodian-creation",
+                    "redacted_fields": [
+                        {
+                            "type": "SingleHeader",
+                            "data": "authorization"
+                        }
+                    ]
+                }
+            ]
+        }
+        p = self.load_policy(policy,
+                             session_factory=session_factory,
+                             config={"region": "us-east-1"})
+
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+
+        client = session_factory().client("wafv2")
+        logging_config = client.get_logging_configuration(ResourceArn=resources[0]["ARN"])
+        self.assertEqual(logging_config["LoggingConfiguration"]["ResourceArn"], resources[0]["ARN"])
+        self.assertEqual(logging_config["LoggingConfiguration"]["LogDestinationConfigs"][0],
+                         "arn:aws:s3:::aws-waf-logs-test-custodian-creation")
+        self.assertIn('RedactedFields', logging_config["LoggingConfiguration"])
+        self.assertEqual(
+            logging_config["LoggingConfiguration"]["RedactedFields"][0]['SingleHeader']['Name'],
+              'authorization')
+
+    def test_wafv2_enable_logging_invalid_scope(self):
+        session_factory = self.replay_flight_data("test_wafv2_enable_logging_invalid_scope")
+        policy = {
+            "name": "wafv2-enable-logging-invalid-scope",
+            "resource": "aws.wafv2",
+            "filters": [
+                {
+                    "type": "value",
+                    "key": "Name",
+                    "value": "test-custodian-waf",
+                    "op": "eq"
+                }
+            ],
+            "actions": [
+                {
+                    "type": "enable-logging",
+                    "log_destination_arn": "arn:aws:s3:::aws-waf-logs-test-custodian-creation",
+                    "log_scope": "INVALID_SCOPE"
+                }
+            ]
+        }
+        p = self.load_policy(policy,
+                             session_factory=session_factory,
+                             config={"region": "us-east-1"})
+
+        with self.assertRaises(ValueError) as context:
+            p.run()
+        self.assertIn("Invalid log_scope value", str(context.exception))
+
+    def test_wafv2_enable_logging_with_logging_filter(self):
+        session_factory = self.replay_flight_data("test_wafv2_enable_logging_with_logging_filter")
+        policy = {
+            "name": "wafv2-enable-logging-with-filter",
+            "resource": "aws.wafv2",
+            "filters": [
+                {
+                    "type": "value",
+                    "key": "Name",
+                    "value": "test-custodian-waf",
+                    "op": "eq"
+                }
+            ],
+            "actions": [
+                {
+                    "type": "enable-logging",
+                    "log_destination_arn": "arn:aws:s3:::aws-waf-logs-test-custodian-creation",
+                    "logging_filter": {
+                        "Filters": [
+                            {
+                                "Behavior": "KEEP",
+                                "Requirement": "MEETS_ALL",
+                                "Conditions": [
+                                    {
+                                        "ActionCondition": {
+                                            "Action": "ALLOW"
+                                        }
+                                    }
+                                ]
+                            }
+                        ],
+                        "DefaultBehavior": "DROP"
+                    }
+                }
+            ]
+        }
+        p = self.load_policy(policy,
+                             session_factory=session_factory,
+                             config={"region": "us-east-1"})
+
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+
+        client = session_factory().client("wafv2")
+        logging_config = client.get_logging_configuration(ResourceArn=resources[0]["ARN"])
+        self.assertEqual(logging_config["LoggingConfiguration"]["ResourceArn"], resources[0]["ARN"])
+        self.assertEqual(logging_config["LoggingConfiguration"]["LogDestinationConfigs"][0],
+                         "arn:aws:s3:::aws-waf-logs-test-custodian-creation")
+        self.assertIn('LoggingFilter', logging_config["LoggingConfiguration"])
+        self.assertEqual(logging_config["LoggingConfiguration"]["LoggingFilter"]["DefaultBehavior"],
+                          "DROP")
