44676e40d3e804e3fdf607de5dfbad78c278c36e
Kapil Thangavelu <kapilt@gmail.com>
2025-01-15 18:17:52 -0500

    workgroup tests

--- tests/test_athena.py
+++ tests/test_athena.py
@@ -2,6 +2,29 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from pytest_terraform import terraform
+from .zpill import ACCOUNT_ID
+
+
+@terraform("athena_workgroup")
+def test_athena_work_group(test, athena_workgroup):
+    factory = test.replay_flight_data("test_athena_work_group")
+    policy = test.load_policy(
+        {"name": "test-athena-work-group",
+         "resource": "aws.athena-workgroup",
+         "filters": [{"Name": athena_workgroup["aws_athena_workgroup.working.name"]}]
+         },
+        config={'account_id': ACCOUNT_ID},
+        session_factory=factory
+    )
+
+    resources = policy.run()
+    assert len(resources) == 1
+    tag_map = {t['Key']: t['Value'] for t in resources[0]['Tags']}
+    assert tag_map == {
+        'App': 'c7n-test',
+        'Env': 'Dev',
+        'Name': 'something'
+    }
 
 
 @terraform("athena_named_query")
