6b9170a899e622efcf31cfeeb810ab4f1ab7627d
Matthew Heidelbaugh <matt9hsix@gmail.com>
2025-03-21 16:43:31 -0400

    add tests

--- c7n/resources/ecr.py
+++ c7n/resources/ecr.py
@@ -394,12 +394,13 @@ def lifecycle_rule_validate(policy, rule):
     #
     # https://docs.aws.amazon.com/AmazonECR/latest/userguide/LifecyclePolicies.html#lp_evaluation_rules
 
-    if (rule['selection']['tagStatus'] == 'tagged' and
-            'tagPrefixList' not in rule['selection']):
-        raise PolicyValidationError(
-            ("{} has invalid lifecycle rule {} tagPrefixList "
-             "required for tagStatus: tagged").format(
-                 policy.name, rule))
+    if rule['selection']['tagStatus'] == 'tagged':
+        if ('tagPrefixList' not in rule['selection'] and
+        'tagPatternList' not in rule['selection']):
+            raise PolicyValidationError(
+                ("{} has invalid lifecycle rule {} tagPrefixList or tagPatternList "
+                "required for tagStatus: tagged").format(
+                    policy.name, rule))
     if (rule['selection']['countType'] == 'sinceImagePushed' and
             'countUnit' not in rule['selection']):
         raise PolicyValidationError(
