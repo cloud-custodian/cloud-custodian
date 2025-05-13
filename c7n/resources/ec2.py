7c10c61275d378e6cb736a35233d813b4a4ee1ae
b177y <bbromell@amazon.com>
2025-05-13 08:33:16 +0000

    feat: use boto.get_partition_for_region instead of hardcoded map

--- c7n/resources/ec2.py
+++ c7n/resources/ec2.py
@@ -1287,7 +1287,7 @@ class InstanceFinding(PostFinding):
         instance = {
             "Type": self.resource_type,
             "Id": "arn:{}:ec2:{}:{}:instance/{}".format(
-                utils.REGION_PARTITION_MAP.get(self.manager.config.region, 'aws'),
+                utils.get_partition(self.manager.config.region),
                 self.manager.config.region,
                 self.manager.config.account_id,
                 r["InstanceId"]),
@@ -1955,8 +1955,8 @@ class AutorecoverAlarm(BaseAction):
                 ActionsEnabled=True,
                 AlarmActions=[
                     'arn:{}:automate:{}:ec2:recover'.format(
-                        utils.REGION_PARTITION_MAP.get(
-                            self.manager.config.region, 'aws'),
+                        utils.get_partition(
+                            self.manager.config.region),
                         i['Placement']['AvailabilityZone'][:-1])
                 ],
                 MetricName='StatusCheckFailed_System',
