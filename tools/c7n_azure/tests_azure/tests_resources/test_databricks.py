dcf497319a5a5d3c702c40b58e3538669aa4f52a
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2025-07-01 00:48:49 +0300

    Add tests

--- tools/c7n_azure/tests_azure/tests_resources/test_databricks.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_databricks.py
@@ -30,3 +30,38 @@ class DatabricksTest(BaseTest):
         })
         resources = p.run()
         self.assertEqual(len(resources), 1)
+
+
+class DatabricksVnetFilterTest(BaseTest):
+    def test_databricks_vnet_filter_schema_prefix(self):
+        p = self.load_policy({
+            'name': 'test-azure-databricks-vnet-filter',
+            'resource': 'azure.databricks',
+            'filters': [
+                {'type': 'vnet',
+                 'key': 'properties.addressSpace.addressPrefixes',
+                 'op': 'contains',
+                 'value': '10.0.0.0/16'}],
+        }, validate=True)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0]['c7n:Vnet']['properties']['addressSpace']['addressPrefixes'], ['10.0.0.0/16'])
+
+
+class DatabricksSubnetsFilterTest(BaseTest):
+    def test_databricks_vnet_filter_schema_prefix(self):
+        p = self.load_policy({
+            'name': 'test-azure-databricks-vnet-filter',
+            'resource': 'azure.databricks',
+            'filters': [{
+                'type': 'subnets',
+                'attrs': [{
+                     'type': 'value',
+                     'key': 'properties.addressPrefix',
+                     'value': '10.0.2.0/24'
+                 }]
+            }]
+        }, validate=True)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0]['c7n:Subnets'][1]['properties']['addressPrefix'], '10.0.2.0/24')
